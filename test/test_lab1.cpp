d919ab780b3f1e94d4615b4652d1146506370ff5
Michail <mdmurzinov@edu.hse.ru>
2020-09-15 12:26:41 +0300

    MORE AND MORE ZEROES IN DELTA

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -17,3 +17,12 @@ TEST(task1, test_zero)
 	ASSERT_DOUBLE_EQ(0.0, calc(0.0));
 }
 
+TEST(task1, test_one)
+{
+	ASSERT_DOUBLE_EQ(1.0, calc(1.0));
+}
+
+TEST(task1, test3)
+{
+	ASSERT_DOUBLE_EQ(9.0, calc(81.0));
+}
