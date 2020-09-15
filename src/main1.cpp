0bd9bf8d6d9192dd02b972b4f9f999419b2e33a5
VarginDimitry <dimitry.vargin@yandex.ru>
2020-09-15 13:21:33 +0300

    V1.0.2

--- src/main1.cpp
+++ src/main1.cpp
@@ -1,5 +1,7 @@
 #include "task1.h"
 
 int main(){
-    cout << calc(256);
+    cout << "sqr of 228 is 15.09966\n";
+    cout << "My answer is " << calc(228) << '\n';
+    cout << (15.09966 == calc(228));
 }
