83ddc506450b7b50ee11d47a9b7ed5c5aeaec507
Alyona Uskova <aluskova01@yandex.ru>
2020-09-15 12:41:53 +0300

    Uskova b27

---
+++ src/main1.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "task1.h"
+
+int main()
+{
+	double result = calc(2.0);
+	printf("Sqrt(%lf)=%lf\n", 2.0, result);
+	return 0;
+}
--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -2,4 +2,61 @@
 
 #include "gtest/gtest.h"
 
+TEST(task1, test_average)
+{
+	ASSERT_NEAR(58.38, average(40.5, 35.76));
 
+}
+
+TEST(task1, test_average)
+{
+	ASSERT_DOUBLE_EQ(0, average(0, 0));
+
+}
+
+TEST(task1, test_good)
+{
+	ASSERT_DOUBLE_EQ(false, good(8.74, 67.3876));
+
+}
+
+TEST(task1, test_good)
+{
+	ASSERT_NEAR(true, good(0.000000003, 0));
+
+}
+
+TEST(task1, test_improve)
+{
+	ASSERT_NEAR(7.365, improve(2.0, 25.46));
+
+}
+
+TEST(task1, test_improve)
+{
+	ASSERT_NEAR(13.53005, improve(25.0601, 50.1202));
+
+}
+
+TEST(task1, test_iter)
+{
+	ASSERT_NEAR(0.000000003, iter(0.000000003, 0));
+
+}
+
+TEST(task1, test_iter)
+{
+	ASSERT_DOUBLE_EQ(0.0000000000000045, iter(0.000000000000009,0));
+
+}
+
+TEST(task1, test_calc)
+{
+	ASSERT_DOUBLE_EQ(3, calc(2));
+
+}
+
+
+
+
+			
