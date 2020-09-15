342e0997af10ab21d362eb3d55f453e49420d97f
m_khorosh <marina.khoroshilova@gmail.com>
2020-09-15 17:21:39 +0300

    fix tests

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -4,12 +4,12 @@
 
 TEST(task1, test1)
 {
-	ASSERT_DOUBLE_EQ(20.0000,average(10.0,30.0);
+	ASSERT_DOUBLE_EQ(20.0,average(10.0,30.0));
 }
 
 TEST(task1, test2)
 {
-	ASSERT_DOUBLE_EQ(433.60803,average(859.366,7.85006);
+	ASSERT_DOUBLE_EQ(48.403,average(89.0,7.806));
 }
 
 TEST(task1, test3)
@@ -24,31 +24,31 @@ TEST(task1, test4)
 
 TEST(task1, test5)
 {
-	ASSERT_DOUBLE_EQ(0,impove(0,0));
+	ASSERT_DOUBLE_EQ(15.2575,improve(30.0,15.45));
 }
 
 TEST(task1, test6)
 {
-	ASSERT_DOUBLE_EQ(0,improve(0,0));
+	ASSERT_DOUBLE_EQ(450.003,improve(900.0,5.1));
 }
 
 TEST(task1, test7)
 {
-	ASSERT_DOUBLE_EQ(1.0,iter(1.0,1.0));
+	ASSERT_DOUBLE_EQ(3.87298,iter(1.0,15.0));
 }
 
 TEST(task1, test8)
 {
-	ASSERT_DOUBLE_EQ(0.0,iter(0.0,0.0));
+	ASSERT_DOUBLE_EQ(1.58114,iter(11.0,2.5));
 }
 
 TEST(task1, test9)
 {
-	ASSERT_DOUBLE_EQ(925.724041,calc(856.965));
+	ASSERT_DOUBLE_EQ(29.274,calc(856.965));
 }
 
 TEST(task1, test10)
 {
-	ASSERT_DOUBLE_EQ(7.00499822,calc(49.07));
+	ASSERT_DOUBLE_EQ(7.005,calc(49.07));
 }
 
