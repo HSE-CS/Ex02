31b9f857e33c14d39a62dfef7e8587912bb648a5
Olesya Nikolaeva <63335416+olesyani@users.noreply.github.com>
2020-09-15 12:25:20 +0300

    b19

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -4,31 +4,31 @@
 
 TEST(task1, test1)
 {
-    ASSERT_DOUBLE_EQ(1.732051, calc(3.0));
+    ASSERT_DOUBLE_EQ(1.732, calc(3.0));
 
 }
 
 TEST(task1, test2)
 {
-     ASSERT_DOUBLE_EQ(2.236068, calc(5.0));
+     ASSERT_DOUBLE_EQ(2.236, calc(5.0));
     
 }
 
 TEST(task1, test3)
 {
-     ASSERT_DOUBLE_EQ(2.345208, calc(5.5));
+     ASSERT_DOUBLE_EQ(2.345, calc(5.5));
     
 }
  
 TEST(task1, test4)
  {
-     ASSERT_DOUBLE_EQ(2.489980, calc(6.2));
+     ASSERT_DOUBLE_EQ(2.489, calc(6.2));
 
  }
 
  TEST(task1, test5)
  {
-      ASSERT_DOUBLE_EQ(2.664583, calc(7.1));
+      ASSERT_DOUBLE_EQ(2.664, calc(7.1));
      
  }
 
@@ -40,24 +40,24 @@ TEST(task1, test4)
  
 TEST(task1, test7)
  {
-     ASSERT_DOUBLE_EQ(3.316625, calc(11.0));
+     ASSERT_DOUBLE_EQ(3.316, calc(11.0));
 
  }
 
  TEST(task1, test8)
  {
-      ASSERT_DOUBLE_EQ(5.567764, calc(31.0));
+      ASSERT_DOUBLE_EQ(5.567, calc(31.0));
      
  }
 
  TEST(task1, test9)
  {
-      ASSERT_DOUBLE_EQ(10.954451, calc(120.0));
+      ASSERT_DOUBLE_EQ(10.954, calc(120.0));
      
  }
 
  TEST(task1, test10)
  {
-      ASSERT_DOUBLE_EQ(14.525839, calc(211.0));
+      ASSERT_DOUBLE_EQ(14.525, calc(211.0));
      
  }
