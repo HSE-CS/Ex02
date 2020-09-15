e7fa8205a65007a075a00b4f8899ed1d78bc2320
Michail <mdmurzinov@edu.hse.ru>
2020-09-15 12:10:17 +0300

    Murzinov Michail b18 initial commit

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -1,5 +1,19 @@
 #include "task1.h"
-
 #include "gtest/gtest.h"
 
+
+TEST(task1, test_average)
+{
+	ASSERT_EQ(2.0, calc(4.0));
+}
+
+TEST(task1, test2)
+{
+	ASSERT_EQ(4.0, calc(16.0))
+}
+
+TEST(task1, test_zero)
+{
+	ASSERT_EQ(0.0, calc(0.0))
+}
 
