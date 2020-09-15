4ad79246565e5f5a286d08369acc8cabce048cbf
Alexey <alekzz2002@yandex.ru>
2020-09-15 12:59:31 +0300

    Klimov b14

--- src/task1.cpp
+++ src/task1.cpp
@@ -1,4 +1,4 @@
-#include <math.h>
+#include <cmath>
 #include <iostream>
 #include "task1.h"
 
@@ -7,7 +7,7 @@ double average(double x, double y) {
 }
 
 double good(double guess, double x) {
-	return abs(guess*guess-x)<delta;
+	return fabs(guess*guess-x)<delta;
 }
 
 double improve(double guess, double x) {
