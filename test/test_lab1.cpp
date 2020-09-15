9db470570717ca852599b1127498357e55b51283
Michail <mdmurzinov@edu.hse.ru>
2020-09-15 12:33:23 +0300

    hot fix

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -14,7 +14,7 @@ TEST(task1, test2)
 
 TEST(task1, test_zero)
 {
-	ASSERT_DOUBLE_EQ(0.0, calc(0.0));
+	ASSERT_NEAR(0.0, calc(0.0), 0.00001);
 }
 
 TEST(task1, test_one)
@@ -25,4 +25,29 @@ TEST(task1, test_one)
 TEST(task1, test3)
 {
 	ASSERT_DOUBLE_EQ(9.0, calc(81.0));
+}
+
+TEST(task1, test_4)
+{
+	ASSERT_NEAR(2.0, calc(4.0), 0.00001);
+}
+
+TEST(task1, 5)
+{
+	ASSERT_NEAR(1.41421, calc(2.0), 0.00001);
+}
+
+TEST(task1, 6)
+{
+	ASSERT_NEAR(5.0, calc(25.0), 0.00001);
+}
+
+TEST(task1, test7)
+{
+	ASSERT_NEAR(10.0, calc(100.0), 0.00001);
+}
+
+TEST(task1, test8)
+{
+	ASSERT_NEAR(7.0, calc(49.0), 0.00001);
 }
