80d4c0ed7498c31aef0f22b47283005a02a6e9ab
TrueTroop <truetroop@mail.ru>
2020-09-15 15:52:02 +0300

    change tests

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -13,7 +13,7 @@ ASSERT_NEAR(4.0, calc(16.0), 0.000001);
 
 TEST(task1, testCalc3)
 {
-ASSERT_NEAR(1.4142135, calc(2.0), 0.000001);
+ASSERT_NEAR(1.414213562, calc(2.0), 0.000001);
 }
 
 TEST(task1, testCalc4)
@@ -36,6 +36,11 @@ TEST(task1, testGood2)
 ASSERT_FALSE(good(5.0, 29.0));
 }
 
+TEST(task1, testGood3)
+{
+ASSERT_TRUE(good(12.0, 144.0));
+}
+
 TEST(task1, testAverage1)
 {
 ASSERT_DOUBLE_EQ(2.5, average(3.0, 2.0));
@@ -46,12 +51,3 @@ TEST(task1, testAverage2)
 ASSERT_NEAR(10.0, average(15.0, 5.0), 0.000001);
 }
 
-TEST(task1, testImprove1)
-{
-ASSERT_DOUBLE_EQ(3.4, improve(5.0, 9));
-}
-
-TEST(task1, testImprove1)
-{
-ASSERT_NEAR(5.19117647, improve(8.5, 16), 0.000001);
-}
