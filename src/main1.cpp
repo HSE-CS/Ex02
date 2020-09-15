f8b5ee66a7a60b353ff310b5cf5f136194370e6a
Sergey Boryaev <somsom.som2@yandex.ru>
2020-09-15 11:38:15 +0300

    add tests

---
+++ src/main1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "task1.h"
+
+int main()
+{
+    double input;
+    scanf("%lf", input);
+    double result = calc(input)
+    printf("%lf.2", result);
+    return 0;
+}
--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -2,4 +2,20 @@
 
 #include "gtest/gtest.h"
 
+TEST(task1, test1)
+{
+    ASSERT_EQ(2.00,getPrime(4));
 
+}
+
+TEST(task1, test2)
+{
+     ASSERT_EQ(1.00,getPrime(1));
+	
+}
+
+TEST(task1, test3)
+{
+     ASSERT_EQ(3.00,getPrime(9));
+	
+}
