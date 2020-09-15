8dfdefe004c85e6d22d31a3d92439652f23099ab
Ksuvot <ksuvot36@yandex.ru>
2020-09-15 11:57:32 +0300

    update files

--- include/task1.h
+++ include/task1.h
@@ -1,1 +1,12 @@
-#pragma once
+#ifndef TASK1_H
+#define TASK1_H
+
+double delta = 0.00000001;
+
+double average(double x, double y);
+bool good(double guess, double x);
+double improve(double guess, double x);
+double iter(double guess, double x);
+double calc(double arg);
+
+#endif // !TASK1_H
