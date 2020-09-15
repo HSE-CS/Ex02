80f4640b7ac0937f423a56ed18881641071db923
AlexShi23 <alieksandr_shirokov@list.ru>
2020-09-15 16:12:31 +0300

    final

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -18,6 +18,11 @@ TEST(task1, test_good1)
 	ASSERT_DOUBLE_EQ(false, good(3, 1));
 }
 
+TEST(task1, test_improve)
+{
+	ASSERT_DOUBLE_EQ(3.0, improve(5.0, 5.0));
+}
+
 TEST(task1, test_iter)
 {
 	ASSERT_DOUBLE_EQ(0.00002, iter(0.00002, 0.000000004));
@@ -25,4 +30,17 @@ TEST(task1, test_iter)
 TEST(task1, test_iter2)
 {
 	ASSERT_NEAR(2.449490, iter(9, 6), 0.000001);
+}
+
+TEST(task1, test_calc1)
+{
+	ASSERT_NEAR(3.0, calc(9.0), 0.000001);
+}
+TEST(task1, test_calc2)
+{
+	ASSERT_NEAR(4.0, calc(16.0), 0.0000001);
+}
+TEST(task1, test_calc3)
+{
+	ASSERT_NEAR(5.0, calc(25.0), 0.000001);
 }
