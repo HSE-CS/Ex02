29e191599f7f6fcbda8eb48799312e47b28aa1eb
TatyanaKonina <63141760+TatyanaKonina@users.noreply.github.com>
2020-09-15 15:39:52 +0300

    Update test_lab1.cpp

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -1,7 +1,5 @@
 #include "task1.h"
-
 #include "gtest/gtest.h"
-
 
 TEST(task1, test1)
 {
@@ -11,7 +9,7 @@ TEST(task1, test1)
 
 TEST(task1, test2)
 {
-    ASSERT_DOUBLE_EQ(40.0, calc(6.324555));
+    ASSERT_DOUBLE_EQ(6.324555, calc(40.0));
 
 }
 
@@ -24,7 +22,10 @@ TEST(task1, test3)
       ASSERT_TRUE(good(1.0,1.0))
 
 }
- Test(task1,test5){
-      ASSERT_FALSE(good(1.0,1.1))
+ Test(task1, test5) {
+     ASSERT_FALSE(good(1.0, 1.1))
 
-}
+ }
+ Test(task1, test6) {
+     ASSERT_DOUBLE_EQ(7.5, average(10.0, 5.0));
+ }
