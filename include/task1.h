3178a8045901531f564bd58ae31bfa444e777404
Ksuvot <ksuvot36@yandex.ru>
2020-09-15 13:32:30 +0300

    fix

--- include/task1.h
+++ include/task1.h
@@ -1,8 +1,7 @@
 #ifndef TASK1_H
 #define TASK1_H
 
-double delta = 0.00000001;
-
+double my_abs(double x);
 double average(double x, double y);
 bool good(double guess, double x);
 double improve(double guess, double x);
--- src/task1.cpp
+++ src/task1.cpp
@@ -1,11 +1,28 @@
 #include "task1.h"
 #include <cmath>
 
-double average(double x, double y) { return (x + y) / 2.0; }
+double delta = 0.00000001;
 
-bool good(double guess, double x) { return abs(guess * guess - x) < delta; }
+double my_abs(double x) {
+	if (x > 0)
+		return x;
+	return -x;
+}
 
-double improve(double guess, double x) { return average(guess, x / guess); }
+double average(double x, double y)
+{
+	return (x + y) / 2.0;
+}
+
+bool good(double guess, double x)
+{
+	return my_abs(guess * guess - x) < delta;
+}
+
+double improve(double guess, double x)
+{
+	return average(guess, x / guess);
+}
 
 double iter(double guess, double x)
 {
@@ -15,4 +32,7 @@ double iter(double guess, double x)
 		return iter(improve(guess, x), x);
 }
 
-double calc(double arg) { return iter(1.0, arg); }
+double calc(double arg)
+{
+	return iter(1.0, arg);
+}
