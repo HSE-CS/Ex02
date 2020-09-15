9669b4946a4a45084ce83ae76fb7ad8de8a8158d
Pasha <paemshanov@edu.hse.ru>
2020-09-15 12:29:10 +0300

    fixed a bit

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -3,13 +3,13 @@
 
 TEST(task1, _average1)
 {
-    ASSERT_DOUBL_EQ(77.5, average(100.0, 55.0));
+    ASSERT_DOUBLE_EQ(77.5, average(100.0, 55.0));
 
 }
 
 TEST(task1, _average2)
 {
-    ASSERT_DOUBL_EQ(83.5, average(167.0, 0.0));
+    ASSERT_DOUBLE_EQ(83.5, average(167.0, 0.0));
 
 }
 
@@ -25,33 +25,33 @@ TEST(task1, _good2)
 }
 TEST(task1, _improve1)
 {
-    ASSERT_DOUBL_EQ(28.0, improve(1.0, 55.0));
+    ASSERT_DOUBLE_EQ(28.0, improve(1.0, 55.0));
 
 }
 TEST(task1, _improve2)
 {
-    ASSERT_DOUBL_EQ(16.0, improve(2.0, 60.0));
+    ASSERT_DOUBLE_EQ(16.0, improve(2.0, 60.0));
 
 }
 TEST(task1, _iter1)
 {
-    ASSERT_DOUBL_EQ(5.0, iter(5.0, 25.0));
+    ASSERT_DOUBLE_EQ(5.0, iter(5.0, 25.0));
 
 }
 TEST(task1, _iter2)
 {
-    ASSERT_DOUBL_EQ(7.416198, iter(28.0, 55.0));
+    ASSERT_DOUBLE_EQ(7.416198, iter(28.0, 55.0));
 
 }
 
 TEST(task1, _calc1)
 {
-    ASSERT_DOUBL_EQ(5.0, calc(25.0));
+    ASSERT_DOUBLE_EQ(5.0, calc(25.0));
 
 }
 
 TEST(task1, _calc2)
 {
-    ASSERT_DOUBL_EQ(1.732051, calc(3.0));
+    ASSERT_DOUBLE_EQ(1.732051, calc(3.0));
 
 }
