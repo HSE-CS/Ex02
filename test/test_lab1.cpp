84a2f832afd6c4914c18609fca96b1cf9f25c89d
JGMax <maksus20.05@mail.ru>
2020-09-15 16:10:05 +0300

    WORK IT

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -7,22 +7,22 @@ TEST(average, test1)
     ASSERT_DOUBLE_EQ(31.5, average(15.0, 48.0));
 }
 
-/*TEST(average, test2)
+TEST(average, test2)
 {
     ASSERT_DOUBLE_EQ(35.0, average(35.0, 35.0));
 }
 
 TEST(improve, test1)
 {
-    ASSERT_DOUBLE_EQ(7.76666665, improve(15.0, 8.0))
+    ASSERT_DOUBLE_EQ(7.76666665, improve(15.0, 8.0));
 }
 
 TEST(improve, test2)
 {
-    ASSERT_DOUBLE_EQ(3.0, improve(1.0, 5.0))
+    ASSERT_DOUBLE_EQ(3.0, improve(1.0, 5.0));
 }
 
 TEST(iter, test1)
 {
-    ASSERT_DOUBLE_EQ(7.79, iter(7.79, 60.6841))
-}*/
+    ASSERT_DOUBLE_EQ(7.79, iter(7.79, 60.6841));
+}
