1887e7c908926cab0b5e04b5b0f90c2a24486743
Nikita Ziganshin <63304439+Liberator-1775@users.noreply.github.com>
2020-09-15 22:43:23 +0300

    Add files via upload

---
+++ include/task1.h
@@ -0,0 +1,19 @@
+//
+// Created by nikita on 15.09.2020.
+//
+
+#ifndef EX02_TASK1_H
+#define EX02_TASK1_H
+
+#include <cmath>
+#include <cstdio>
+
+double delta = 0.00000001;
+
+double average (double x,double y);
+bool good(double guess,double x);
+double improve (double guess,double x);
+double iter(double guess, double x);
+double calc(double arg);
+
+#endif //EX02_TASK1_H
