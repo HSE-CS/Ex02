7ef907b31e52a380c48cbe3802eae8dd31616f4c
Michail <mdmurzinov@edu.hse.ru>
2020-09-15 19:52:01 +0300

    New tests

--- include/task1.h
+++ include/task1.h
@@ -3,4 +3,12 @@
 
 double calc(double arg);
 
+double average(double x, double y);
+
+bool good(double guess, double x);
+
+double improve(double guess, double x);
+
+double iter(double guess, double x);
+
 #endif // !TASK1_H
