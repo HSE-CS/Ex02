da7c359babdab93b8ff6ea66c2cf4e698ebb0dbb
LukichevaPolina <lukichevapolina42@gmail.com>
2020-09-15 12:21:53 +0300

    add Ex02 with tests Lukicheva Polina

---
+++ src/main1.cpp
@@ -0,0 +1,8 @@
+#include <math.h>
+#include <stdio.h>
+#include "task1.h"
+int main() {
+	double result = calc(0.99999);
+	printf("Sqrt(%lf)=%lf\n", 2.0, result);
+	return 0;
+}
--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -1,5 +1,56 @@
-#include "task1.h"
-
-#include "gtest/gtest.h"
-
-
+//#include "task1.h"
+//
+//#include "gtest/gtest.h"
+//
+//TEST(task1, test1Average)
+//{
+//	ASSERT_EQ(1.5, average(1.0, 2.0));
+//
+//}
+//
+//TEST(task1, test1Good)
+//{
+//	ASSERT_EQ(true, good(10, 1));
+//
+//}
+//
+//TEST(task1, test2Good)
+//{
+//	ASSERT_EQ(false, good(0.000002, 0.0000000000001));
+//
+//}
+//TEST(task1, test1Improve)
+//{
+//	ASSERT_EQ(5.25, improve(10, 5));
+//
+//}
+//TEST(task1, test2Improve)
+//{
+//	ASSERT_EQ(3.857143, improve(7, 5));
+//
+//}
+//TEST(task1, test1Iter)
+//{
+//	ASSERT_EQ(10, iter(10, 1));
+//
+//}
+//TEST(task1, test2Iter)
+//{
+//	ASSERT_EQ(0.000548, iter(0.000001, 0.0000003));
+//
+//}
+//TEST(task1, test1Calc)
+//{
+//	ASSERT_EQ(2.645751, calc(7));
+//
+//}
+//TEST(task1, test2Calc)
+//{
+//	ASSERT_EQ(0.910055, calc(0.8282));
+//
+//}
+//TEST(task1, test3Calc)
+//{
+//	ASSERT_EQ(0.910055, calc(0.99999));
+//
+//}
