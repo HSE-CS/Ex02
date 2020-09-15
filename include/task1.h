f763e4c2275a6190362dcb9ffe308f5ff3999f51
Gerasimov Alexey <gerasimov2122@icloud.com>
2020-09-15 12:18:45 +0300

    tests added

---
+++ include/task1.h
@@ -0,0 +1,15 @@
+
+#ifndef TASK1_TASK1_H
+#define TASK1_TASK1_H
+
+double average(double x, double y);
+
+bool good(double guess, double x);
+
+double improve(double guess, double x);
+
+double iter(double guess, double x);
+
+double calc(double arg);
+
+#endif //TASK1_TASK1_H
--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -2,4 +2,47 @@
 
 #include "gtest/gtest.h"
 
+TEST(task1, average1
+){
+ASSERT_DOUBLE_EQ(69.1919,average(83.838383, 54.545454));
+}
+TEST(task1, average2
+){
+ASSERT_DOUBLE_EQ(3.0,average(2.0, 4.0));
+}
+TEST(task1, good1
+){
+ASSERT_EQ(1,good(5.5, 30.25));
+}
+TEST(task1, good2
+){
+ASSERT_EQ(0,good(342.123, 234.123));
+}
+TEST(task1, improve1
+){
+ASSERT_DOUBLE_EQ(5.32475,improve(10.1, 5.55));
+}
+TEST(task1, improve2
+){
+ASSERT_DOUBLE_EQ(22.2632,improve(44.404, 5.4321));
+}
+TEST(task1, iter1
+){
+ASSERT_DOUBLE_EQ(2.33069,iter(44.404, 5.4321));
 
+}
+TEST(task1, iter2
+){
+ASSERT_DOUBLE_EQ(72.2664,iter(8321.1234, 5222.4321));
+
+}
+TEST(task1, calc1
+){
+ASSERT_DOUBLE_EQ(91.2202,calc(8321.1234));
+
+}
+TEST(task1, calc2
+){
+ASSERT_DOUBLE_EQ(111.111,calc(12345.6789));
+
+}
