d017b8ecf6b5cdb3e7e1822c8a4b4f4fcb97532c
Mushka Nikita <namushka@edu.hse.ru>
2020-09-15 15:52:51 +0300

    3rd try

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -5,3 +5,30 @@ TEST(task1, test1)
 {
     ASSERT_NEAR(2.449490, calc(6.0), 0.00001);
 }
+
+TEST(task1, test2)
+{
+    ASSERT_NEAR(2.000000, calc(4.0), 0.00001);
+}
+
+TEST(task1, test3)
+{
+    ASSERT_NEAR(3.000000, calc(9.0), 0.00001);
+}
+
+TEST(task1, test4)
+{
+    ASSERT_NEAR(1.000000, average(1.0,1.0), 0.00001);
+}
+
+
+TEST(task1, test5)
+{
+    ASSERT_NEAR(6.000000, calc(5.0,7.0), 0.00001);
+}
+
+
+TEST(task1, test6)
+{
+    ASSERT_NEAR(3.000000, calc(9.0), 0.00001);
+}
