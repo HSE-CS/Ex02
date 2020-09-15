e16497bb17a9a806f889526dd8769e56aada148a
Olesya Nikolaeva <63335416+olesyani@users.noreply.github.com>
2020-09-15 11:59:04 +0300

    b19

---
+++ include/task1.h
@@ -0,0 +1,13 @@
+#ifndef task1_h
+#define task1_h
+
+#include <iostream>
+#include <math.h>
+
+double average(double x, double y);
+bool good(double guess, double x);
+double improve(double guess, double x);
+double iter(double guess, double x);
+double calc(double arg);
+
+#endif
