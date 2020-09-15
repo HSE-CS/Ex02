4157df1db163069d3c1bf53fe7fd4e527e4cd6ea
sharg <jenya.sharunov@gmail.com>
2020-09-15 15:41:55 +0300

    3add ASSERT_NEAR delta

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -2,24 +2,24 @@
 #include "gtest/gtest.h"
 TEST(task1, test1)
 {
-ASSERT_NEAR(1.73205081,calc(3));
+ASSERT_NEAR(1.73205081,calc(3),0.0000001);
 }
 
 TEST(task1, test2)
 {
-ASSERT_NEAR(3.16227766,calc(10));
+ASSERT_NEAR(3.16227766,calc(10),0.0000001);
 
 }
 
 TEST(task1, test3)
 {
-ASSERT_NEAR(11.26942767,calc(127));
+ASSERT_NEAR(11.26942767,calc(127),0.0000001);
 }
 
 
 TEST(task1, test4)
 {
-ASSERT_NEAR(2.50000000,average(2.0,3.0));
+ASSERT_NEAR(2.50000000,average(2.0,3.0),0.0000001);
 }
 
 TEST(task1, test5)
@@ -42,16 +42,16 @@ ASSERT_EQ(true, good(2.50000000,1));
 
 TEST(task1, test8)
 {
-ASSERT_NEAR(2.50000000, improve(2.0,6.0));
+ASSERT_NEAR(2.50000000, improve(2.0,6.0),0.0000001);
 }
 
 TEST(task1, test9)
 {
-ASSERT_NEAR(16.03121954,calc(257.0));
+ASSERT_NEAR(16.03121954,calc(257.0),0.0000001);
 }
 
 TEST(task1, test10)
 {
-ASSERT_NEAR(9.89949494, calc(98));
+ASSERT_NEAR(9.89949494, calc(98),0.0000001);
 }
 
