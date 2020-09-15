d74c54df17f7b35c4f5d390c83ef8756f6234368
Irina <tytylk14@gmail.com>
2020-09-15 17:58:09 +0300

    reduced accuracy

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -4,12 +4,12 @@
 
 TEST(task1, test1)
 {
-    ASSERT_NEAR(2.236067977, calc(5), 0.00000001);
+    ASSERT_NEAR(2.236067977, calc(5), 0.0001);
 }
 
 TEST(task1, test2)
 {
-    ASSERT_NEAR(3.0, average(2.0, 4.0), 0.00000001);
+    ASSERT_NEAR(3.0, average(2.0, 4.0), 0.0001);
 }
 
 TEST(task1, test3)
@@ -24,7 +24,7 @@ TEST(task1, test4)
 
 TEST(task1, test5)
 {
-    ASSERT_NEAR(3.0, calc(9), 0.00000001);
+    ASSERT_NEAR(3.0, calc(9), 0.0001);
 }
 
 TEST(task1, test6)
@@ -34,7 +34,7 @@ TEST(task1, test6)
 
 TEST(task1, test7)
 {
-    ASSERT_NEAR(2.645751, calc(7), 0.00000001);
+    ASSERT_NEAR(2.645751, calc(7), 0.0001);
 }
 
 TEST(task1, test8)
