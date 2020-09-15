3a08d35ca88b98476aab53ab6252b2fa31646662
jswark <swark.a.j@gmail.com>
2020-09-15 17:12:25 +0300

    fix test

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -27,7 +27,7 @@ TEST(task1, test2_good){
 }
 
 TEST(task1, test1_improve){
-    ASSERT_NEAR(39.149, improve(77.0, 100.0), 0.00001);
+    ASSERT_DOUBLE_EQ(51.0, improve(50.0, 100.0));
 }
 
 TEST(task1, test2_improve){
