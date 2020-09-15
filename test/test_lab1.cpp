b0ab916edeb72ebd4e515935723d5f1832fbfef1
sand user <sand09.me@gmail.com>
2020-09-15 12:27:32 +0300

    Sapozhnikov v0.5

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -2,15 +2,15 @@
 #include "task1.h"
 
 TEST(task1, test_for_4){
-    ASSERT_DOUBLE_EQ(2.0, calc(4.0))
+    ASSERT_NEAR(2.0, calc(4.0), 0.00001);
 }
 
 TEST(task1, test_for_9){
-    ASSERT_DOUBLE_EQ(3.0, calc(9.0))
+    ASSERT_NEAR(3.0, calc(9.0), 0.00001);
 }
 
 TEST(task1, test_for_81){
-    ASSERT_DOUBLE_EQ(9.0, calc(81.0))
+    ASSERT_NEAR(9.0, calc(81.0), 0.00001);
 }
 
 
