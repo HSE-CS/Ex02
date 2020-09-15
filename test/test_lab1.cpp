7e779372b2250d732378808a4ad32e357542ebad
TatyanaKonina <63141760+TatyanaKonina@users.noreply.github.com>
2020-09-15 15:47:10 +0300

    Update test_lab1.cpp

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -3,29 +3,18 @@
 
 TEST(task1, test1)
 {
-    ASSERT_DOUBLE_EQ(5.0, calc(25.0));
-
+	
+	ASSERT_NEAR(1.4142135, calc(2.0), 0.000001);
 }
 
 TEST(task1, test2)
 {
-    ASSERT_DOUBLE_EQ(6.324555, calc(40.0));
-
+	
+	ASSERT_NEAR(5.0, calc(25.0), 0.000001);
 }
 
 TEST(task1, test3)
 {
-    ASSERT_DOUBLE_EQ(0.0, average(0.0));
-
+	
+	ASSERT_NEAR(4.58257569, calc(21.0), 0.000001);
 }
- Test(task1,test4){
-      ASSERT_TRUE(good(1.0,1.0))
-
-}
- Test(task1, test5) {
-     ASSERT_FALSE(good(1.0, 1.1))
-
- }
- Test(task1, test6) {
-     ASSERT_DOUBLE_EQ(7.5, average(10.0, 5.0));
- }
