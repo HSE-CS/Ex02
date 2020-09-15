c2bb2c92994103f43e55ffaf9a434bae678f242c
Michail <mdmurzinov@edu.hse.ru>
2020-09-15 20:02:24 +0300

    hot fix

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -2,67 +2,67 @@
 #include "gtest/gtest.h"
 
 
-TEST(test_for_calc_function, test_for_4)
+TEST(task1, test_for_4)
 {
 	ASSERT_NEAR(2.0, calc(4.0), 0.00001);
 }
 
-TEST(test_for_calc_function, test_for_16)
+TEST(task1, test_for_16)
 {
 	ASSERT_NEAR(4.0, calc(16.0), 0.00001);
 }
 
-TEST(test_for_calc_function, test_zero)
+TEST(task1, test_zero)
 {
 	ASSERT_NEAR(0.0, calc(0.0), 0.01);
 }
 
-TEST(test_for_calc_function, test_for_one)
+TEST(task1, test_for_one)
 {
 	ASSERT_NEAR(1.0, calc(1.0), 0.00001);
 }
 
-TEST(test_for_calc_function, test_for_2)
+TEST(task1, test_for_2)
 {
 	ASSERT_NEAR(1.41421, calc(2.0), 0.00001);
 }
 
-TEST(test_for_average_function, test1)
+TEST(task1, test1)
 {
-	ASSER_NEAR(4.0, average(5.0, 3.0), 0.1);
+	ASSERT_NEAR(4.0, average(5.0, 3.0), 0.1);
 }
 
-TEST(test_for_average_function, test2)
+TEST(task1, test2)
 {
-	ASSER_NEAR(1.5, average(1.0, 2.0), 0.1);
+	ASSERT_NEAR(1.5, average(1.0, 2.0), 0.1);
 }
 
-TEST(test_for_good_function, test1)
+TEST(task1, test3)
 {
 	ASSERT_TRUE(good(1.0, 1.0));
 }
 
-TEST(test_for_good_function, test2)
+TEST(task1, test4)
 {
 	ASSERT_FALSE(good(1.0, 1.0));
 }
 
-TEST(test_for_improve_function, test2)
+TEST(task1, test5)
 {
 	ASSERT_NEAR(2.25, improve(4.0, 2.0), 0.01);
 }
 
-TEST(test_for_improve_function, test2)
+TEST(task1, test6)
 {
 	ASSERT_NEAR(5.1, improve(4.0, 2.0), 0.01);
 }
 
-TEST(test_for_iter_function, test2)
+TEST(task1, test7)
 {
 	ASSERT_NEAR(1.41421, iter(3.0, 2.0), 0.00001);
 }
 
-TEST(test_for_iter_function, test2)
+TEST(task1, test8)
 {
 	ASSERT_NEAR(3.162278, iter(3.0, 10.0), 0.000001);
 }
