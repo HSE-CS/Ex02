72a8d55f6bbe22bb0d6e7ee172415aa83fe2c34f
JGMax <maksus20.05@mail.ru>
2020-09-15 15:23:33 +0300

    Bredikhin Max

---
+++ include/task1.h
@@ -0,0 +1,15 @@
+//
+// Created by jmax on 15.09.2020.
+//
+#ifndef EX_2_TASK1_H
+#define EX_2_TASK1_H
+
+#include <math.h>
+#include <stdio.h>
+double average(double x, double y);
+bool good(double guess, double x);
+double improve(double guess, double x);
+double iter(double guess, double x);
+double calc(double arg);
+
+#endif //EX_2_TASK1_H
