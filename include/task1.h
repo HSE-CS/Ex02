64d5b68eb49ddcab46cbb749491775e5a8b54158
wityat <ttrttr449@gmail.com>
2020-09-15 15:24:06 +0300

    first commit with tests

---
+++ include/task1.h
@@ -0,0 +1,12 @@
+//
+// Created by victor on 15.09.2020.
+//
+
+#ifndef TASK1_TASK1_H
+#define TASK1_TASK1_H
+double average (double x,double y);
+bool good(double guess,double x);
+double improve(double guess,double x);
+double iter(double guess, double x);
+double calc(double arg);
+#endif //TASK1_TASK1_H
--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -2,4 +2,48 @@
 
 #include "gtest/gtest.h"
 
+TEST(task1, test1)
+{
+    ASSERT_EQ(3.16227766, calc(10));
+}
 
+TEST(task1, test7)
+{
+ASSERT_EQ(2.82842712, calc(8));
+}
+
+TEST(task1, test2)
+{
+    ASSERT_EQ(1.4167, improve(1.5, 2));
+}
+
+TEST(task1, test6)
+{
+ASSERT_EQ(1.4142, improve(1.4167, 2));
+}
+
+TEST(task1, test3)
+{
+    ASSERT_EQ(false, good(1, 2));
+}
+
+TEST(task1, test4)
+{
+    ASSERT_EQ(true, good(2.82842712, 8));
+}
+TEST(task1, test5)
+{
+    ASSERT_EQ(2.0, average(2, 2));
+}
+TEST(task1, test8)
+{
+    ASSERT_EQ(3.0, average(3, 3));
+}
+TEST(task1, test9)
+{
+    ASSERT_EQ(4.0, average(4, 4));
+}
+TEST(task1, test10)
+{
+    ASSERT_EQ(3.31662479, calc(11));
+}
