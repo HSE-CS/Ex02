a2fa1571f434943ae84eccb1f56616a6d334e6c3
VadMack <makarovvadim01@icloud.com>
2020-09-15 12:14:01 +0300

    done

---
+++ src/main1.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Vadim Makarov on 15.09.2020.
+//
+#include <stdio.h>
+#include "task1.h"
+
+
+int main(){
+    double result = calc(2.0);
+    printf("Sqrt(%lf)=%lf\n", 2.0, result);
+    return 0;
+}
+
--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -2,4 +2,47 @@
 
 #include "gtest/gtest.h"
 
+TEST(task1, test_average1){
+    ASSERT_DOUBLE_EQ(1.000000, average(1.0, 1.0));
+}
+
+TEST(task1, test_average2){
+    ASSERT_DOUBLE_EQ(2.500000, average(3.0, 2.0));
+}
+
+TEST(task1, test_good1){
+    ASSERT_EQ(1, good(5.0, 25.0));
+}
+
+TEST(task1, test_good2){
+    ASSERT_EQ(0, good(455.0, 666.0));
+}
+
+TEST(task1, test_improve1){
+    ASSERT_DOUBLE_EQ(228.231868, improve(455.0, 666.0));
+}
+
+TEST(task1, test_improve2){
+    ASSERT_DOUBLE_EQ(200.250000, improve(400.0, 200.0));
+}
+
+TEST(task1, test_iter1){
+    ASSERT_DOUBLE_EQ(14.142136, iter(400.0, 200.0));
+}
+
+TEST(task1, test_iter2){
+    ASSERT_DOUBLE_EQ(1.414214, iter(14.0, 2.0));
+}
+
+TEST(task1, test_calc1){
+    ASSERT_DOUBLE_EQ(3.741657, calc(14.0));
+}
+
+TEST(task1, test_calc2){
+    ASSERT_DOUBLE_EQ(2.449490, calc(6.0));
+}
+
+
+
+
 
