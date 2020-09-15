4bb8735d53d3dd821b9c8c2be5ba8aa66c656d1f
sand user <sand09.me@gmail.com>
2020-09-15 12:37:28 +0300

    Sapozhnikov 1.0 10 tests added

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -13,4 +13,30 @@ TEST(task1, test_for_81){
     ASSERT_NEAR(9.0, calc(81.0), 0.00001);
 }
 
+TEST(task1, test_for_1){
+    ASSERT_NEAR(1.0, calc(1.0), 0.00001);
+}
 
+TEST(task1, test_for_2){
+    ASSERT_NEAR(1.414214, calc(2.0), 0.00001);
+}
+
+TEST(task1, test_for_5){
+    ASSERT_NEAR(2.236068, calc(5.0), 0.00001);
+}
+
+TEST(task1, test_for_289){
+    ASSERT_NEAR(17.0, calc(289.0), 0.00001);
+}
+
+TEST(task1, test_for_100){
+    ASSERT_NEAR(10.0, calc(100.0), 0.00001);
+}
+
+TEST(task1, test_for_05){
+    ASSERT_NEAR(0.707107, calc(0.5), 0.00001);
+}
+
+TEST(task1, test_for_12){
+    ASSERT_NEAR(3.464102, calc(12.0), 0.00001);
+}
