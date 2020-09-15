8529eb828aa729464864b47f251a05408ad37b4e
Michail <mdmurzinov@edu.hse.ru>
2020-09-15 12:13:39 +0300

    Fixed tests b18

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -4,16 +4,16 @@
 
 TEST(task1, test_average)
 {
-	ASSERT_EQ(2.0, calc(4.0));
+	ASSERT_DOUBLE_EQ(2.0, calc(4.0));
 }
 
 TEST(task1, test2)
 {
-	ASSERT_EQ(4.0, calc(16.0))
+	ASSERT_DOUBLE_EQ(4.0, calc(16.0))
 }
 
 TEST(task1, test_zero)
 {
-	ASSERT_EQ(0.0, calc(0.0))
+	ASSERT_DOUBLE_EQ(0.0, calc(0.0))
 }
 
