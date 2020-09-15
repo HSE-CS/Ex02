8bbc4126071053425bad98262f771f47cb9f0dfb
Alexey <alekzz2002@yandex.ru>
2020-09-15 13:15:30 +0300

    Klimov b14fixx

--- src/main1.cpp
+++ src/main1.cpp
@@ -2,8 +2,7 @@
 #include "task1.h"
 
 int main() {
-//	double result=calc(123.0);
-//	printf("Sqrt(%lf)=%lf\n",2.0,result);
-	std::cout << iter(1.41421, 2);
+	double result=calc(123.0);
+	printf("Sqrt(%lf)=%lf\n",2.0,result);
 	return 0;
 }
