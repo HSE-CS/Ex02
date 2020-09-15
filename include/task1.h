574dd2a9dd3d2c5456eb74e04d1054270830bf72
JGMax <maksus20.05@mail.ru>
2020-09-15 15:58:07 +0300

    WORK IT

--- include/task1.h
+++ include/task1.h
@@ -4,8 +4,6 @@
 #ifndef EX_2_TASK1_H
 #define EX_2_TASK1_H
 
-#include <math.h>
-#include <stdio.h>
 double average(double x, double y);
 bool good(double guess, double x);
 double improve(double guess, double x);
--- src/task1.cpp
+++ src/task1.cpp
@@ -1,14 +1,18 @@
 //
 // Created by jmax on 15.09.2020.
 //
-#include "task1.h"
+#include <iostream>
+using namespace std;
 
 double delta=0.00000001;
 double average(double x, double y) {
     return (x+y)/2.0;
 }
 bool good(double guess, double x) {
-    return abs(guess * guess - x) < delta;
+    if (guess * guess - x >= 0)
+        return guess * guess - x < delta;
+    else
+        return -1 * (guess * guess - x) < delta;
 }
 double improve(double guess, double x) {
     return average(guess,x/guess);
