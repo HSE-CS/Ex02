ee52197140f9abd9f0a78b9350d9ae6df0a31ea0
Ksuvot <ksuvot36@yandex.ru>
2020-09-15 13:49:34 +0300

    test average

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -9,8 +9,15 @@ TEST(task1, test_calc_2) {
     ASSERT_NEAR(2.82842712, calc(8.0), 0.00000001);
 }
 
+TEST(task1, test_average_1) {
+    ASSERT_NEAR(1.5, average(1, 2), 0.00000001);
+}
+
+TEST(task1, test_average_2) {
+    ASSERT_NEAR(3, average(1, 5), 0.00000001);
+}
+
 // average
 // good
 // improve
 // iter
-// calc
