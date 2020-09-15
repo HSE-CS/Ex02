a4b2ce95208c9ec89f87cadba13ba6b3a1f2db93
Irina <tytylk14@gmail.com>
2020-09-15 22:04:28 +0300

    try again vs2

--- include/task1.h
+++ include/task1.h
@@ -8,6 +8,6 @@ double average(double x, double y);
 bool good(double guess, double x);
 double improve(double guess, double x);
 double iter(double guess, double x);
-double calc(double arg);
+double calc(double arg); 
 
 #endif
