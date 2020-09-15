39a26fba80909168b83ce7dd98a0e9256c880d26
Sergey Boryaev <somsom.som2@yandex.ru>
2020-09-15 12:02:17 +0300

    fix tests

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -4,18 +4,18 @@
 
 TEST(task1, test1)
 {
-    ASSERT_EQ(2.00,calc(4));
+    ASSERT_EQ(2,calc(4));
 
 }
 
 TEST(task1, test2)
 {
-    ASSERT_EQ(1.00,calc(1));
+    ASSERT_EQ(1,calc(1));
 	
 }
 
 TEST(task1, test3)
 {
-    ASSERT_EQ(3.00,calc(9));
+    ASSERT_EQ(3,calc(9));
 	
 }
