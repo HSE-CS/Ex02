d400cf38c965433ef4ade0b04b74e40d04cd6760
Stanislav Stoianov <stanis.stoyanov@outlook.com>
2020-09-15 16:03:09 +0300

    add header

--- include/task1.h
+++ include/task1.h
@@ -5,4 +5,18 @@
 #ifndef TASK1_TASK1_H
 #define TASK1_TASK1_H
 
+#include <iostream>
+
+double abs_custom(double x);
+
+double average(double x, double y);
+
+bool good(double guess, double x);
+
+double improve(double guess, double x);
+
+double iter(double guess, double x);
+
+double calc(double arg);
+
 #endif //TASK1_TASK1_H
