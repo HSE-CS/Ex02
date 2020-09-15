af362b90525a776f35bb2a2edbb2eb28be11721e
Osmanov Islam <osmanv152@gmail.com>
2020-09-15 12:10:31 +0300

    Osmanov v0.3

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -5,19 +5,19 @@
 TEST(task1, test_average)
 {
 
-ASSERT_EQ(2.00, calc(4.0));
+ASSERT_DOUBLE_EQ(2.00, calc(4.0));
 
 }
 
 TEST(task1, test2)
 {
-ASSERT_EQ(4.0,calc(16.0));
+ASSERT_DOUBLE_EQ(4.0,calc(16.0));
 
 }
 
 TEST(task1, test3)
 {
-ASSERT_EQ(5.0,calc(25.0));
+ASSERT_DOUBLE_EQ(5.0,calc(25.0));
 
 }
 
