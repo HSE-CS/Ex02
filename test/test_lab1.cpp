d8090d0980ab6ee2781a3f42fa4a00ef7d1ccdb4
sharg <jenya.sharunov@gmail.com>
2020-09-15 15:33:35 +0300

    add ASSERT_NEAR

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -2,24 +2,24 @@
 #include "gtest/gtest.h"
 TEST(task1, test1)
 {
-ASSERT_EQ(1.73205081,calc(3));
+ASSERT_NEAR(1.73205081,calc(3));
 }
 
 TEST(task1, test2)
 {
-ASSERT_EQ(3.16227766,calc(10));
+ASSERT_NEAR(3.16227766,calc(10));
 
 }
 
 TEST(task1, test3)
 {
-ASSERT_EQ(127,calc(11.26942767));
+ASSERT_NEAR(11.26942767,calc(127));
 }
 
 
 TEST(task1, test4)
 {
-ASSERT_EQ(2.50000000,average(2.0,3.0));
+ASSERT_NEAR(2.50000000,average(2.0,3.0));
 }
 
 TEST(task1, test5)
@@ -42,16 +42,16 @@ ASSERT_EQ(true, good(2.50000000,1));
 
 TEST(task1, test8)
 {
-ASSERT_EQ(2.50000000, improve(2.0,6.0));
+ASSERT_NEAR(2.50000000, improve(2.0,6.0));
 }
 
 TEST(task1, test9)
 {
-ASSERT_EQ(16.03121954,calc(257.0));
+ASSERT_NEAR(16.03121954,calc(257.0));
 }
 
 TEST(task1, test10)
 {
-ASSERT_EQ(9.89949494, calc(98));
+ASSERT_NEAR(9.89949494, calc(98));
 }
 
