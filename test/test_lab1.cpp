5899bcd16054b74ad84c96c53c748a8e445eb5ea
Ksuvot <ksuvot36@yandex.ru>
2020-09-15 13:40:02 +0300

    test calc

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -1,6 +1,16 @@
 #include "task1.h"
 #include "gtest/gtest.h"
 
-TEST(task1, test1) {
+TEST(task1, test_calc_1) {
     ASSERT_NEAR(2.0, calc(4.0), 0.00000001);
 }
+
+TEST(task1, test_calc_2) {
+    ASSERT_NEAR(2.82842712, calc(8.0), 0.00000001);
+}
+
+// average
+// good
+// improve
+// iter
+// calc
