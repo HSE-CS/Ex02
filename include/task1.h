02c9b74cd7c285aaf5ab35f4500720ac14d4b0c4
Osmanov Islam <osmanv152@gmail.com>
2020-09-15 11:55:21 +0300

    Osmanov v0.1

---
+++ include/task1.h
@@ -0,0 +1,9 @@
+//
+// Created by islam on 15.09.2020.
+//
+
+#ifndef WORK_TASK1_H
+#define WORK_TASK1_H
+
+double calc(double arg);
+#endif //WORK_TASK1_H
--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -2,4 +2,22 @@
 
 #include "gtest/gtest.h"
 
+TEST(task1, test_average)
+{
+
+ASSERT_EQ(2.00, calc(4.0));
+
+}
+
+TEST(task1, test2)
+{
+ASSERT_EQ(4.0,calc(16.0));
+
+}
+
+TEST(task1, test3)
+{
+ASSERT_EQ(5.0,calc(25.0));
+
+}
 
