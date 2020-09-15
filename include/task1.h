0437aeba71c6badea32981e41ed71f6b7ae22dd1
Andrej <andrej_kostin@mail.ru>
2020-09-15 16:05:01 +0300

    Kostin Andrej 0.4 version 10 test

--- include/task1.h
+++ include/task1.h
@@ -1,9 +1,10 @@
-#include <stdio.h>
-#include <math.h>
-using namespace std;
+#ifndef TASK1_TASK1_H
+#define TASK1_TASK1_H
 
-double average(double x, double y);
-bool good(double guess, double x);
-double improve(double guess, double x);
+double calc(double arg);
+double average (double x,double y);
+bool good(double guess,double x);
+double improve(double guess,double x);
 double iter(double guess, double x);
 double calc(double arg);
+#endif //TASK1_TASK1_H
--- src/task1.cpp
+++ src/task1.cpp
@@ -1,26 +1,22 @@
-#include <stdio.h>
 #include <cmath>
-
-double delta = 0.00000001;
-double average(double x, double y) {
-    return (x + y) / 2.0;
+#include "task1.h"
+double delta=0.00000001;
+using namespace std;
+double average (double x,double y) {
+    return (x+y) /2.0;
 }
-
-bool good(double guess, double x) {
-    return abs(guess * guess - x) < delta;
+bool good(double guess,double x) {
+    return abs (guess*guess-x) <delta;
 }
-
-double improve(double guess, double x) {
-    return average(guess, x / guess);
+double improve(double guess,double x) {
+    return average (guess, x/guess) ;
 }
-
 double iter(double guess, double x) {
-    if (good(guess, x))
+    if (good (guess, x) )
         return guess;
     else
-        return iter(improve(guess, x), x);
+        return iter (improve (guess,x),x);
 }
 double calc(double arg) {
-    return iter(1.0, arg);
+    return iter(1.0,arg);
 }
-
