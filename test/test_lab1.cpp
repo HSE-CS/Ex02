d8694d4b9d57d2cff3b83953759040e2d0658f00
Osmanov Islam <osmanv152@gmail.com>
2020-09-15 12:33:54 +0300

    Osmanov v0.6

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -17,7 +17,58 @@ ASSERT_DOUBLE_EQ(4.0,calc(16.0));
 
 TEST(task1, test3)
 {
+
 ASSERT_DOUBLE_EQ(5.0,calc(25.0));
 
 }
+
+TEST(task1, test3)
+{
+
+ASSERT_DOUBLE_EQ(3.0,calc(9.0));
+
+}
+
+TEST(task1, test3)
+{
+
+ASSERT_DOUBLE_EQ(1.0,calc(1.0));
+
+}
+
+TEST(task1, test3)
+{
+
+ASSERT_NEAR(1.73205,calc(3), 0.0001);
+
+}
+TEST(task1, test3)
+{
+
+ASSERT_NEAR(1.41421,calc(2), 0.0001);
+
+}
+
+TEST(task1, test3)
+{
+
+ASSERT_NEAR(2.23606,calc(5), 0.0001);
+
+}
+
+TEST(task1, test3)
+{
+
+ASSERT_NEAR(6.0,calc(36.0), 0.0001);
+
+}
+
+TEST(task1, test3)
+{
+
+ASSERT_NEAR(7.0,calc(49.0), 0.0001);
+
+}
+
+
 
