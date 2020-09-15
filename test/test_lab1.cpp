4d27b129b2852c4846f69a74759a36bf983bf7aa
VarginDimitry <dimitry.vargin@yandex.ru>
2020-09-15 12:43:27 +0300

    Test 4

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -1,42 +1,42 @@
 #include "task1.h"
 #include "gtest/gtest.h"
 
-TEST(task1, test_1){
-    ASSERT_DOUBLE_EQ(2.0, calc(4.0));
+TEST(task1, test1){
+    ASSERT_NEAR(2.0, calc(4.0), 0.000001);
 }
 
-TEST(task1, test_2){
-    ASSERT_DOUBLE_EQ(16.0, calc(256.0));
+TEST(task1, test2){
+    ASSERT_NEAR(1.41421356, calc(2.0), 0.000001);
 }
 
-TEST(task1, test_3){
-    ASSERT_DOUBLE_EQ(0, calc(0.0));
+TEST(task1, test3){
+    ASSERT_NEAR(0, calc(5.0), 0.000001);
 }
 
-TEST(task1, test_4){
-    ASSERT_DOUBLE_EQ(0, calc(0.0));
+TEST(task1, test4){
+    ASSERT_NEAR(0, calc(0.0), 0.000001);
 }
 
-TEST(task1, test_5){
-    ASSERT_DOUBLE_EQ(0, calc(0.0));
+TEST(task1, test5){
+    ASSERT_NEAR(0, calc(0.0), 0.000001);
 }
 
-TEST(task1, test_6){
-    ASSERT_DOUBLE_EQ(0, calc(0.0));
+TEST(task1, test6){
+    ASSERT_NEAR(0, calc(0.0), 0.000001);
 }
 
-TEST(task1, test_7){
-    ASSERT_DOUBLE_EQ(0, calc(0.0));
+TEST(task1, test7){
+    ASSERT_NEAR(0, calc(0.0), 0.000001);
 }
 
-TEST(task1, test_8){
-    ASSERT_DOUBLE_EQ(0, calc(0.0));
+TEST(task1, test8){
+    ASSERT_NEAR(0, calc(0.0), 0.000001);
 }
 
-TEST(task1, test_9){
-    ASSERT_DOUBLE_EQ(0, calc(0.0));
+TEST(task1, test9){
+    ASSERT_NEAR(0, calc(0.0), 0.000001);
 }
 
-TEST(task1, test_10){
-    ASSERT_DOUBLE_EQ(0, calc(0.0));
+TEST(task1, test10){
+    ASSERT_NEAR(0, calc(0.0), 0.000001);
 }
