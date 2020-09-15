83a0da13b3360ac3ed9ef00ba1155c91549c827b
Igor Tyulin <grizlylol@ya.ru>
2020-09-15 15:54:55 +0300

    B-52

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -4,50 +4,68 @@
 
 TEST(task1, test1)
 {
-    ASSERT_DOUBLE_EQ(2.0, calc(4.0));
+
+ASSERT_DOUBLE_EQ(1.0, calc(1.0));
+
 }
 
 TEST(task1, test2)
 {
-    ASSERT_DOUBLE_EQ(1.0, calc(1.0));
+ASSERT_DOUBLE_EQ(2.0,calc(4.0));
+
 }
 
 TEST(task1, test3)
 {
-    ASSERT_DOUBLE_EQ(6.0, calc(36.0));
+
+ASSERT_DOUBLE_EQ(3.0,calc(9.0));
+
 }
 
 TEST(task1, test4)
 {
-    ASSERT_DOUBLE_EQ(10.0, calc(100.0));
+
+ASSERT_DOUBLE_EQ(4.0,calc(16.0));
+
 }
 
 TEST(task1, test5)
 {
-    ASSERT_NEAR(1.567, calc(3), 0.0001);
+
+ASSERT_DOUBLE_EQ(5.0,calc(25.0));
+
 }
 
 TEST(task1, test6)
 {
-    ASSERT_NEAR(1.321, calc(2), 0.0001);
-}
 
+ASSERT_NEAR(1.66666,calc(3), 0.0001);
+
+}
 TEST(task1, test7)
 {
-    ASSERT_NEAR(2.543, calc(6), 0,0001);
+
+ASSERT_NEAR(1.32322,calc(2), 0.0001);
+
 }
 
 TEST(task1, test8)
 {
-    ASSERT_TRUE(good(6.25, 39.0625));
+
+ASSERT_NEAR(2.78863,calc(8), 0.0001);
+
 }
 
 TEST(task1, test9)
 {
-    ASSERT_FALSE(good(4,3));
+
+ASSERT_TRUE(good(2,4));
+
 }
 
 TEST(task1, test10)
 {
-    ASSERT_TRUE(good(17, 289));
+
+ASSERT_FALSE(good(16, 256));
+
 }
