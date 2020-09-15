6d404e42e3a8f214d7b8024153662201641e8373
wityat <ttrttr449@gmail.com>
2020-09-15 15:32:57 +0300

    bug with abs2

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -7,31 +7,31 @@ TEST(task1, test1)
     ASSERT_EQ(3.16227766, calc(10));
 }
 
-TEST(task1, test7)
+TEST(task1, test2)
 {
 ASSERT_EQ(2.82842712, calc(8));
 }
 
-TEST(task1, test2)
+TEST(task1, test3)
 {
     ASSERT_EQ(1.4167, improve(1.5, 2));
 }
 
-TEST(task1, test6)
+TEST(task1, test4)
 {
 ASSERT_EQ(1.4142, improve(1.4167, 2));
 }
 
-TEST(task1, test3)
+TEST(task1, test5)
 {
     ASSERT_EQ(false, good(1, 2));
 }
 
-TEST(task1, test4)
+TEST(task1, test6)
 {
     ASSERT_EQ(true, good(2.82842712, 8));
 }
-TEST(task1, test5)
+TEST(task1, test7)
 {
     ASSERT_EQ(2.0, average(2, 2));
 }
