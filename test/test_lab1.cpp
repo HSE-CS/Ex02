b166318c9ffef70e3b0c49039039406263b94b79
Ksuvot <ksuvot36@yandex.ru>
2020-09-15 13:23:58 +0300

    fix

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -1,13 +1,6 @@
 #include "task1.h"
 #include "gtest/gtest.h"
 
-TEST(task1, test_average_1)
-{
-	ASSERT_DOUBLE_EQ(1.5, average(2.0, 1.0));
-
-}
-TEST(task1, test_average_2)
-{
-	ASSERT_DOUBLE_EQ(3.0, average(2.0, 4.0));
-
+TEST(task1, test1) {
+    ASSERT_NEAR(2.0, calc(4.0), 0.00000001);
 }
