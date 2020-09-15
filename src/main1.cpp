1b901f76217497a226bccb51a1a8f71982d7d99a
Grigory <greg.smirnov2013@yandex.ru>
2020-09-15 12:01:01 +0300

    test of some fuctions

---
+++ src/main1.cpp
@@ -0,0 +1,10 @@
+#include <math.h>
+#include <stdio.h>
+#include "task1.h"
+
+int main()
+{
+	double result = calc(2.0);
+	printf("%lf", result);
+	return 0;
+}
--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -2,4 +2,32 @@
 
 #include "gtest/gtest.h"
 
+TEST_average5(task1, test1)
+{
+    ASSERT_EQ(5.0,average(3.0,7.0));
 
+}
+
+TEST_average7(task1, test2)
+{
+    ASSERT_EQ(7.5, average(0,15);
+
+}
+
+TEST_calc4(task1, test3)
+{
+    ASSERT_EQ(4.0, calc(16.0);
+
+}
+
+TEST_calc2(task1, test4)
+{
+    ASSERT_EQ(2.0, calc(4.0);
+
+}
+
+TEST_good0(task1, test4)
+{
+    ASSERT_EQ(0,good(10,);
+
+}
