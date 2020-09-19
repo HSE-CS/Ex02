e41f8ab1b2c3ae7c43c6455a829827b7ab149b84
Alyona Uskova <aluskova01@yandex.ru>
2020-09-19 14:09:32 +0300

    Uskova b27

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -2,61 +2,61 @@
 
 #include "gtest/gtest.h"
 
-TEST(task1, test_average)
+TEST(task1, test_average1)
 {
 	ASSERT_NEAR(38.130000, average(40.5, 35.76), 0.000001);
 
 }
 
-TEST(task1, test_average)
+TEST(task1, test_average2)
 {
 	ASSERT_NEAR(0.000000, average(0, 0), 0.000001);
 
 }
 
-TEST(task1, test_good)
+TEST(task1, test_good1)
 {
 	ASSERT_DOUBLE_EQ(false, good(8.74, 67.3876));
 
 }
 
-TEST(task1, test_good)
+TEST(task1, test_good2)
 {
 	ASSERT_DOUBLE_EQ(true, good(0.000003, 0));
 
 }
 
-TEST(task1, test_improve)
+TEST(task1, test_improve1)
 {
 	ASSERT_NEAR(7.365000, improve(2.0, 25.46), 0.000001);
 
 }
 
-TEST(task1, test_improve)
+TEST(task1, test_improve2)
 {
 	ASSERT_NEAR(13.53005, improve(25.0601, 50.1202), 0.000001);
 
 }
 
-TEST(task1, test_iter)
+TEST(task1, test_iter1)
 {
 	ASSERT_NEAR(0.001500, iter(0.003, 0), 0.000001);
 
 }
 
-TEST(task1, test_iter)
+TEST(task1, test_iter2)
 {
 	ASSERT_NEAR(0.001758, iter(0.09, 0), 0.000001);
 
 }
 
-TEST(task1, test_calc)
+TEST(task1, test_calc1)
 {
 	ASSERT_NEAR(0.001953, calc(0), 0.000001);
 
 }
 
-TEST(task1, test_calc)
+TEST(task1, test_calc2)
 {
 	ASSERT_NEAR(0.003023, calc(0.000007), 0.000001);
 
