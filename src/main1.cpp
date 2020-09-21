5d66742649e7f34661cb19efe2056f69258a0e14
Ivan Dobryaev <ivan.dobryaev@gmail.com>
2020-09-21 13:51:59 +0300

    initial commit

---
+++ src/main1.cpp
@@ -0,0 +1,12 @@
+#include "task1.h"
+#include <iostream>
+
+using namespace std;
+
+
+int main ()
+{
+  double result=calc(2.0);
+  printf("Sqrt (%lf)=$lf\n", 2.0, result);
+  return 0;
+}
--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -3,3 +3,7 @@
 #include "gtest/gtest.h"
 
 
+TEST (task1, test1)
+{
+  ASSERT_NEAR(2.0, 1.41421356, 0.00000001)
+}
