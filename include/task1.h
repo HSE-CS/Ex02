16a116c136c98ff31cd430b634d60556df3cbec2
EgorBuzanov <egor4ek007@gmail.com>
2020-09-21 15:27:09 +0300

    Check 2 tests

---
+++ include/task1.h
@@ -0,0 +1,12 @@
+#ifndef TASK1_H
+#define TASK1_H
+
+double delta = 0.00000001;
+
+double average(double x, double y);
+bool good(double guess, double x);
+double improve(double guess, double x);
+double iter(double guess, double x);
+double calc(double arg);
+
+#endif
--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -2,4 +2,38 @@
 
 #include "gtest/gtest.h"
 
+TEST(task1, test1){
+    ASSERT_DOUBLE_EQ(1.5, average(1.0, 2.0))}
 
+TEST(task1, test2)
+{
+    ASSERT_DOUBLE_EQ(2.0, average(1.0, 3.0))
+}
+
+// TEST(task1, test3){
+//     ASSERT_DOUBLE_EQ(, improve())}
+
+// TEST(task1, test4){
+//     ASSERT_DOUBLE_EQ(, iter())}
+
+// TEST(task1, test5){
+//     ASSERT_EQ(true, good())}
+
+// TEST(task1, test6)
+// {}
+
+// TEST(task1, test7)
+// {
+// }
+
+// TEST(task1, test8)
+// {
+// }
+
+// TEST(task1, test9)
+// {
+// }
+
+// TEST(task1, test10)
+// {
+// }
