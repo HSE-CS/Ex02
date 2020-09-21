0705168349dce12bd92e7991201489527ceafc05
danek0100 <71225718+danek0100@users.noreply.github.com>
2020-09-21 15:56:07 +0300

    3

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -5,18 +5,52 @@
 
 TEST(task1, test1)
 {
-    ASSERT_DOUBLE_EQ(2.0, avearge(2, 2);
-    ASSERT_DOUBLE_EQ(1.5, avearge(3, 2);
-    ASSERT_DOUBLE_EQ(9.5, avearge(10, 9);
+    ASSERT_DOUBLE_EQ(2.0, avearge(2.0, 2.0));
 }
 
-TEST(task1, test1)
+TEST(task1, test2)
 {
-    ASSERT_DOUBLE_EQ(2.0, avearge(2, 2);
-    ASSERT_DOUBLE_EQ(1.5, avearge(3, 2);
-    ASSERT_DOUBLE_EQ(9.5, avearge(10, 9);
+    ASSERT_DOUBLE_EQ(9.5, avearge(10.0, 9.0));
 }
 
+TEST(task1, test3)
+{
+    ASSERT_DOUBLE_EQ(9.5, avearge(10.0, 9.0));
+}
 
+TEST(task1, test4)
+{
+    ASSERT_DOUBLE_EQ(9.5, avearge(10.0, 9.0));
+}
+
+TEST(task1, test5)
+{
+    ASSERT_DOUBLE_EQ(9.5, avearge(10.0, 9.0));
+}
+
+TEST(task1, test6)
+{
+    ASSERT_DOUBLE_EQ(9.5, avearge(10.0, 9.0));
+}
+
+TEST(task1, test7)
+{
+    ASSERT_DOUBLE_EQ(9.5, avearge(10.0, 9.0));
+}
+
+TEST(task1, test8)
+{
+    ASSERT_DOUBLE_EQ(9.5, avearge(10.0, 9.0));
+}
+
+TEST(task1, test9)
+{
+    ASSERT_DOUBLE_EQ(9.5, avearge(10.0, 9.0));
+}
+
+TEST(task1, test10)
+{
+    ASSERT_DOUBLE_EQ(9.5, avearge(10.0, 9.0));
+}
 
 
