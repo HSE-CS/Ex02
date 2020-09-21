6f7698632a887b500ecaf19ed4fef622385cd0df
Anton Klykov <antonklykov01@mail.ru>
2020-09-21 14:17:39 +0300

    Загружаю файлы 2

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -4,31 +4,31 @@
 
 TEST(task1, test1)
 {
-    ASSERT_EQ(3571,average(20.0, 50.0));
+    ASSERT_NEAR(3571,average(20.0, 50.0), 5);
 
 }
 
 TEST(task1, test2)
 {
-     ASSERT_EQ(2,good(30.0, 15.0));
+     ASSERT_NEAR(2,good(30.0, 15.0), 5);
 	
 }
 
 TEST(task1, test3)
 {
-     ASSERT_EQ(3,improve(25.0, 13.0));
+     ASSERT_NEAR(3,improve(25.0, 13.0), 5);
 	
 }
 
 TEST(task1, test3)
 {
-     ASSERT_EQ(5,iter(10.0, 13.0));
+     ASSERT_NEAR(5,iter(10.0, 13.0), 5);
 	
 }
 
 TEST(task1, test3)
 {
-     ASSERT_EQ(10,calc(40.0, 53.0));
+     ASSERT_NEAR(10,calc(40.0, 53.0), 5);
 	
 }
 
