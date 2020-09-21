43da5eb4913f0648895d47e88f077088a1abcc0a
TimurZaytsev <tz200128@gmail.com>
2020-09-21 15:45:10 +0300

    TestOne

---
+++ include/task1.h
@@ -0,0 +1,8 @@
+#ifndef task1
+#define task1
+double calc(double arg);
+double average(double x, double y);
+bool good(double guess, double x);
+double improve(double guess, double x);
+double iter(double guess, double x);
+#endif
