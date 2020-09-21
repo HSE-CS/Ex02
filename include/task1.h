b7b313ca1a9eea1ab635a9acaf9fde4d1089de35
Leonidek23 <rad.shabarshin@mail.ru>
2020-09-21 17:07:18 +0300

    Леонид

--- include/task1.h
+++ include/task1.h
@@ -1,8 +1,5 @@
-#ifndef Task1_H
-#define Task1_H
 double average(double x, double y);
 bool good(double guess, double x);
 double improve(double guess, double x);
 double iter(double guess, double x);
 double calc(double arg);
-#endif
