1609561a9e340cddaeb22caaa098f1217edd776f
k-dash6 <71211839+k-dash6@users.noreply.github.com>
2020-09-21 16:40:27 +0300

    hobana

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -1,5 +1,16 @@
 #include "task1.h"
 
+
 #include "gtest/gtest.h"
 
+//TEST(task1,test1) {
+ //   ASSERT_DOUBLE_EQ(2.0, average(2.0, 2.0));
+//}
+
+
+TEST(task1, test1)
+{
+    ASSERT_NEAR(1.414214, calc(2.0), 0.000001);
+
+}
 
