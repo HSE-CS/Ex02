51d28e41bafbdb649719ed24be3046336b38518d
Nikita Kasyanov <kasyanikita@gmail.com>
2020-09-21 15:10:16 +0300

    Attempt 1

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -1,5 +1,8 @@
-#include "task1.h"
-
+#include "../include/task1.h"
 #include "gtest/gtest.h"
 
+TEST (SquareRootTest, PositiveNos) {
+    ASSERT_DOUBLE_EQ (5.0, average(5,5));
+    ASSERT_DOUBLE_EQ (5.5, average(6,5));
+}
 
