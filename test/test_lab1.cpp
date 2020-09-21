7354430dee8b8ad55d7684c0297fc8af6a345697
HabKaffee <neknaum1@gmail.com>
2020-09-21 15:58:15 +0300

    Test1.2

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -13,6 +13,14 @@ TEST(task1, test1)
     ASSERT_NEAR(1.414214, calc(2.0), 0.000001);
 
 }
+
+TEST(task1, test2)
+{
+    ASSERT_NEAR(1.73205, calc(3.0), 0.00001);
+
+}
+
+
 //
 //TEST(task1, test3)
 //{
