068d172fa8b23cbf8deecf516567f9d13a172d6b
Irina <pasmanikirina29@gmail.com>
2020-09-21 15:57:25 +0300

    tests

--- include/task1.h
+++ include/task1.h
@@ -1,1 +1,18 @@
-#pragma once
+#ifndef TASK1_H
+#define TASK1_H
+#define _CRT_SECURE_NO_WARNINGS
+#include <math.h>
+#include <cmath>
+#include <stdio.h>
+
+double average(double x, double y);
+
+bool good(double guess, double x);
+
+double improve(double guess, double x);
+
+double iter(double guess, double x);
+
+double calc(double arg);
+
+#endif TASK1_H
