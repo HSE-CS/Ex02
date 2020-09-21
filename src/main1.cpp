75bb4f6d7e43ede9741e48ec59b5cf20fcd8c541
Polina <polpan01@mail.ru>
2020-09-21 15:40:20 +0300

    test1

---
+++ src/main1.cpp
@@ -0,0 +1,9 @@
+#include <task1.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main() 
+{
+  double result = calc(2.0);
+  printf("Sqrt(%lf)=%lf\n",2.0,result);
+  return 0;
+}
--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -1,5 +1,8 @@
 #include "task1.h"
-
 #include "gtest/gtest.h"
 
+TEST(task1, test1)
+{
+ASSERT_EQ(1.414214, calc(2.0));
 
+}
