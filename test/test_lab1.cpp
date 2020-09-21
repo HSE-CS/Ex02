79ab44d3ab8948aceb81b07b53eeeb66f2ef2cac
Right-bugor <innokentiyyyyy@gmail.com>
2020-09-21 16:08:40 +0300

    2

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -1,6 +1,42 @@
 #include "task1.h"
 #include "gtest/gtest.h"
 
-TEST(task1, Test-one) {
+TEST(task1, TestOne) {
 	ASSERT_DOUBLE_EQ(calc(2.0), 1.414214);
+}
+
+TEST(task1, TestTwo) {
+	ASSERT_DOUBLE_EQ(calc(4.0), 2.0);
+}
+
+TEST(task1, TestThree) {
+	ASSERT_DOUBLE_EQ(calc(4.0), 2.0);
+}
+
+TEST(task1, TestFour) {
+	ASSERT_DOUBLE_EQ(calc(4.0), 2.0);
+}
+
+TEST(task1, TestFive) {
+	ASSERT_DOUBLE_EQ(calc(4.0), 2.0);
+}
+
+TEST(task1, TestSix) {
+	ASSERT_DOUBLE_EQ(calc(4.0), 2.0);
+}
+
+TEST(task1, TestSeven) {
+	ASSERT_DOUBLE_EQ(calc(4.0), 2.0);
+}
+
+TEST(task1, TestEight) {
+	ASSERT_DOUBLE_EQ(calc(4.0), 2.0);
+}
+
+TEST(task1, TestNine) {
+	ASSERT_EQ(good(3.1415926777, 3.1415926771), true);
+}
+
+TEST(task1, TestTen) {
+	ASSERT_EQ(good(3.1415926, 3.1415929), false);
 }
