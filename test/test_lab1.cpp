2ba6440cb49a47c9314de1108598bb1564b3f819
Vladislav-Kanev <71171071+Vladislav-Kanev@users.noreply.github.com>
2020-09-21 15:34:32 +0300

    ...

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -4,17 +4,18 @@
 
 TEST(task1, test1)
 {
-    ASSERT_EQ(50, calc(2500));
+    ASSERT_NEAR(1.414214, calc(2), 0.00000001);
 
 }
 
 TEST(task1, test2)
 {
-    ASSERT_EQ(2, calc(4));
+    ASSERT_NEAR(1.414214, calc(2), 0.00000001);
 
 }
 
 TEST(task1, test3)
 {
-    ASSERT_EQ(3, calc(9));
+    ASSERT_NEAR(1.414214, calc(2), 0.00000001);
+
 }
