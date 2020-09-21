92776f4071292a2056a2049cbe42d73f7efb5211
KuznetsovMikhail <KuznetsovMikhail>
2020-09-21 19:11:04 +0300

    Кузнецов Михаил

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -4,23 +4,23 @@
 
 TEST(task1, test1)
 {
-    ASSERT_DOUBLE_EQ(2.5,average(2.0, 3.0));
+    ASSERT_NEAR(2.5,average(2.0, 3.0),0.00000001);
 
 }
 
 TEST(task1, test2)
 {
-     ASSERT_DOUBLE_EQ(true,good(1.0, 1.0));
+     ASSERT_NEAR(true,good(1.0, 1.0),0.00000001);
 
 }
 
 TEST(task1, test3)
 {
-     ASSERT_DOUBLE_EQ(1.0,improve(1.0, 1.0));
+     ASSERT_NEAR(1.0,improve(1.0, 1.0),0.00000001);
 
 }
 
-TEST (task1, test4)
+TEST(task1, test4)
 {
-    ASSERT_DOUBLE_EQ(1.41421,calc(2.0));
+    ASSERT_NEAR(1.41421,calc(2.0),0.00000001);
 }
