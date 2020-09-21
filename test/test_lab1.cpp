95b50247d10a2916c6f0151ec12d19da097b4612
TimurZaytsev <tz200128@gmail.com>
2020-09-21 19:59:55 +0300

    OOO

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -3,4 +3,14 @@
 TEST(task1, test1)
 {
     ASSERT_NEAR(1.414214, calc(2.0), 0.000001);
+    ASSERT_NEAR(1.732050, calc(3.0), 0.000001);
+    ASSERT_NEAR(2.0, calc(4.0), 0.000001);
+    ASSERT_NEAR(2.236067, calc(5.0), 0.000001);
+    ASSERT_NEAR(2.449489, calc(6.0), 0.000001);
+    ASSERT_NEAR(2.645751, calc(7.0), 0.000001);
+    ASSERT_NEAR(2.828427, calc(8.0), 0.000001);
+    ASSERT_NEAR(3.0, calc(9.0), 0.000001);
+    ASSERT_NEAR(3.162277, calc(10.0), 0.000001);
+    ASSERT_NEAR(3.316624, calc(11.0), 0.000001);
 }
+
