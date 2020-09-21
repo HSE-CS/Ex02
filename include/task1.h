9d581a11a3bcf9ad7f4c7180b8ec632e1f795762
Void-fun <55204554+Void-fun@users.noreply.github.com>
2020-09-21 14:38:51 +0300

    adad

---
+++ include/task1.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cmath>
+
+double average(double x, double y);
+
+bool good(double guess, double x);
+
+double improve(double guess, double x);
+
+double iter(double guess, double x);
+
+double calc(double arg);
+
