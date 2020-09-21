e2d945a904f5a0679b44ea5a057d685cf021aba9
alexgiving <alexgiving@mail.ru>
2020-09-21 15:36:00 +0300

    aaaaaaaaaaaa

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -11,12 +11,12 @@ ASSERT_NEAR(1.414214, calc(2), 0.00000001);
 
 TEST(task1, test2)
 {
-ASSERT_EQ(2,calc(4), 0.00000001);
+ASSERT_NEAR(2,calc(4), 0.00000001);
 
 }
 
 TEST(task1, test3)
 {
-ASSERT_EQ(3,calc(9), 0.00000001);
+ASSERT_NEAR(3,calc(9), 0.00000001);
 
 }
