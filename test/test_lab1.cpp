2b1f85053b0d80f935d6f06954a1a3f88ca061b1
eakrayushkina57 <eakrayushkina_1@edu.hse.ru>
2020-09-21 22:25:50 +0300

    b103

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -1,57 +1,34 @@
+
 #include "task1.h"
 
 #include "gtest/gtest.h"
 
 TEST(task1, test1)
 {
-    ASSERT_NEAR(4, calc(16), 0.1);
+ASSERT_NEAR(1.414214, calc(2), 0.1);
 
 }
 
 TEST(task1, test2)
 {
-    ASSERT_NEAR(2, calc(4), 0.1);
+ASSERT_NEAR(2.0 ,calc(4), 0.1);
 
 }
 
 TEST(task1, test3)
 {
-    ASSERT_NEAR(3, calc(9), 0.1);
+ASSERT_NEAR(3.0 ,calc(9), 0.1);
 
 }
 
 TEST(task1, test4)
 {
-    ASSERT_NEAR(4, calc(16), 0.1);
+ASSERT_NEAR(25.0 ,average(50,0), 0.1);
 
 }
+
 TEST(task1, test5)
 {
-    ASSERT_NEAR(5, calc(25), 0.1);
-
-}
-TEST(task1, test6)
-{
-    ASSERT_NEAR(6, calc(36), 0.1);
-
-}
-TEST(task1, test7)
-{
-    ASSERT_NEAR(40, calc(1600), 0.1);
-
-}
-TEST(task1, test8)
-{
-    ASSERT_NEAR(8, calc(64), 0.1);
-
-}
-TEST(task1, test9)
-{
-    ASSERT_NEAR(900, calc(30), 0.1);
-
-}
-TEST(task1, test10)
-{
-    ASSERT_NEAR(400, calc(20), 0.1);
+ASSERT_NEAR(2.0 ,average(2,2), 0.1);
 
 }
