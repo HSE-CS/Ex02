0b831f558fdbb7e2ce6788fc7fc5357dd9652e83
EgorBuzanov <egor4ek007@gmail.com>
2020-09-21 15:53:31 +0300

    Proverka

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -2,32 +2,42 @@
 
 #include "gtest/gtest.h"
 
-TEST(task1, test1){
-    ASSERT_DOUBLE_EQ(1.5, average(1.0, 2.0))}
+TEST(task1, test1)
+{
+    ASSERT_FLOAT_EQ(1.5, average(1.0, 2.0));
+}
 
 TEST(task1, test2)
 {
-    ASSERT_DOUBLE_EQ(2.0, average(1.0, 3.0))
+    ASSERT_FLOAT_EQ(2.0, average(1.0, 3.0));
 }
 
-// TEST(task1, test3){
-//     ASSERT_DOUBLE_EQ(, improve())}
+TEST(task1, test3)
+{
+    ASSERT_NEAR(1.41421356, calc(2.0), 0.00000001);
+}
 
-// TEST(task1, test4){
-//     ASSERT_DOUBLE_EQ(, iter())}
+TEST(task1, test4)
+{
+    ASSERT_NEAR(1.41, calc(2.0), 0.01);
+}
 
-// TEST(task1, test5){
-//     ASSERT_EQ(true, good())}
+TEST(task1, test5){
+    ASSERT_DOUBLE_EQ(average(1.0, 2.0 / 1.0), improve(1.0, 2.0))}
 
-// TEST(task1, test6)
-// {}
+TEST(task1, test6)
+{
+    ASSERT_DOUBLE_EQ(average(2.0, 3.0 / 2.0), improve(2.0, 3.0))
+}
 
 // TEST(task1, test7)
 // {
+//     ASSERT_EQ(1, good());
 // }
 
 // TEST(task1, test8)
 // {
+//     ASSERT_EQ(0, good());
 // }
 
 // TEST(task1, test9)
