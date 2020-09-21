084bed5be978578d94d16da890003a68c9d150ba
Right-bugor <innokentiyyyyy@gmail.com>
2020-09-21 20:21:32 +0300

    naconec ||

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -16,18 +16,23 @@ TEST(task1, test1) {
 TEST(task1, test1) {
 	ASSERT_NEAR(2.449489, calc(6.0), 0.000001);
 }
-TEST(task1, test1) {
-	ASSERT_NEAR(2.645751, calc(7.0), 0.000001);
+TEST(task1, test1)
+{
+	ASSERT_DOUBLE_EQ(2.500000, average(2.0, 3.0));
 }
-TEST(task1, test1) {
-	ASSERT_NEAR(2.828427, calc(8.0), 0.000001);
+TEST(task1, test2)
+{
+	ASSERT_EQ(0.000000, good(5.0, 8.0));
 }
-TEST(task1, test1) {
-	ASSERT_NEAR(3.0, calc(9.0), 0.000001);
+TEST(task1, test3)
+{
+	ASSERT_FLOAT_EQ(4.142857, improve(7.0, 9.0));
 }
-TEST(task1, test1) {
-	ASSERT_NEAR(3.162277, calc(10.0), 0.000001);
+TEST(task1, test7)
+{
+	ASSERT_FLOAT_EQ(3.000000, iter(7.0, 9.0));
 }
-TEST(task1, test1) {
-	ASSERT_NEAR(3.316624, calc(11.0), 0.000001);
+TEST(task1, test9)
+{
+	ASSERT_FLOAT_EQ(1.000000, good(0.0, 0.0));
 }
