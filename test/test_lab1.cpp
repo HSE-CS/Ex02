4d747224ff0ff60efce9313c6bb684dd982be566
PolinaZharavina <pdzharavina@edu.hse.ru>
2020-09-21 15:42:57 +0300

    Test1 for calc

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -4,6 +4,12 @@
 
 TEST(task1, test1)
 {
-	ASSERT_EQ(2.500000, average(2.0, 3.0));
+	ASSERT_FLOAT_EQ(2.500000, average(2.0, 3.0));
+
+}
+
+TEST(task1, test2)
+{
+	ASSERT_FLOAT_EQ(1.414214, calc(2.0));
 
 }
