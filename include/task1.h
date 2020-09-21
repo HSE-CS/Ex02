1b463ac99634d3c7cc181dcbbd07ab98d2ad0968
Niki <you@example.com>
2020-09-21 21:08:38 +0300

    welllll

--- include/task1.h
+++ include/task1.h
@@ -1,1 +1,8 @@
-#pragma once
+#define TASK1_H
+#ifdef TASK1_H
+double average(double x, double y);
+bool good(double guess, double x);
+double improve(double guess, double x);
+double iter(double guess, double x);
+double calc(double arg);
+#endif TASK1_H
