6c48038faae3f9dfc7682755ff0e04c041c4c26f
Diana <drsharibzhanova@edu.hse.ru>
2020-09-21 15:34:18 +0300

    Тест2

--- src/main1.cpp
+++ src/main1.cpp
@@ -5,14 +5,8 @@
 
 int main()
 {
-    /*double result = calc(2.0);
+    double result = calc(2.0);
     printf("Sqrt(%lf)=%lf\n", 2.0, result);
-    return(0);*/
-
-    int result = 0;
-    result = good(2.0, 3.45);
-    printf("%lf\n", result);
-    result = good(5.55, 0.45);
-    printf("%lf", result);
+    return(0);
     
 }
