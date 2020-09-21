5e3b6db4ad80433bde39427c7d6e442658a44f74
alexgiving <alexgiving@mail.ru>
2020-09-21 15:10:47 +0300

    tests

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -1,5 +1,22 @@
+
 #include "task1.h"
 
 #include "gtest/gtest.h"
 
+TEST(task1, test1)
+{
+ASSERT_EQ(50,calc(2500));
 
+}
+
+TEST(task1, test2)
+{
+ASSERT_EQ(2,calc(4));
+
+}
+
+TEST(task1, test3)
+{
+ASSERT_EQ(3,calc(9));
+
+}
