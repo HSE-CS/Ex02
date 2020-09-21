9bf248e68249182c24a98bc25cfee3a97bea426b
Kristina Egorova <kristinaeo2001@mail.ru>
2020-09-21 14:21:47 +0300

    B97

---
+++ include/task1.h
@@ -0,0 +1,11 @@
+#ifndef TASK1_H
+#define TASK1_H
+#define _CRT_SECURE_NO_WARNINGS
+#include <math.h>
+#include <cmath>
+#include <stdio.h>
+
+double calc(double arg);
+//int ASSERT_EQ(double x, double y);
+
+#endif TASK1_H
