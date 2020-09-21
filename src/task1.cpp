ce25aa521f5210a6d8cb01e4ff7320487a07b43c
Polina <polpan01@mail.ru>
2020-09-21 16:25:37 +0300

    tests for calc and average

--- src/task1.cpp
+++ src/task1.cpp
@@ -9,7 +9,7 @@ double average(double x, double y)
 }
 bool good(double guess, double x)
 {
-  return fabs(guess * guess - x) < delta;
+  return abs(guess * guess - x) < delta;
 }
 double improve(double guess, double x)
 {
