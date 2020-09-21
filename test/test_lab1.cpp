4ad541f874f550bbca7ed3f1c9f22039143a641a
Right-bugor <innokentiyyyyy@gmail.com>
2020-09-21 20:33:48 +0300

    cto tebe nugno shifrovoi butozer

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -5,3 +5,48 @@ TEST(task1, test1)
 	ASSERT_NEAR(1.414214, calc(2.0), 0.000001);
 
 }
+TEST(task1, test2)
+{
+	ASSERT_NEAR(1.414214, calc(2.0), 0.000001);
+
+}
+TEST(task1, test3)
+{
+	ASSERT_NEAR(1.414214, calc(2.0), 0.000001);
+
+}
+TEST(task1, test4)
+{
+	ASSERT_NEAR(1.414214, calc(2.0), 0.000001);
+
+}
+TEST(task1, test5)
+{
+	ASSERT_NEAR(1.414214, calc(2.0), 0.000001);
+
+}
+TEST(task1, test6)
+{
+	ASSERT_NEAR(1.414214, calc(2.0), 0.000001);
+
+}
+TEST(task1, test7)
+{
+	ASSERT_NEAR(1.414214, calc(2.0), 0.000001);
+
+}
+TEST(task1, test8)
+{
+	ASSERT_NEAR(1.414214, calc(2.0), 0.000001);
+
+}
+TEST(task1, test9)
+{
+	ASSERT_NEAR(1.414214, calc(2.0), 0.000001);
+
+}
+TEST(task1, test10)
+{
+	ASSERT_NEAR(1.414214, calc(2.0), 0.000001);
+
+}
