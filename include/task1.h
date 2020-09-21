f6aed151ad82ef3a81ca4c1743d856b51ebf0673
NAKAUTT <khorev.egor@list.ru>
2020-09-21 14:18:31 +0300

    123

---
+++ include/task1.h
@@ -0,0 +1,21 @@
+//
+// Created by Егор on 21.09.2020.
+//
+
+#ifndef TEST_TASK1_H
+#define TEST_TASK1_H
+
+#include <stdio.h>
+#include <cmath>
+
+double average(double x, double y);
+
+bool good(double guess, double x);
+
+double improve(double guess, double x);
+
+double iter(double guess, double x);
+
+double calc(double arg);
+
+#endif //TEST_TASK1_H
--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -1,5 +1,24 @@
-#include "task1.h"
 
+#include "task1.h"
 #include "gtest/gtest.h"
 
 
+TEST (task1, test1) {
+ASSERT_EQ(2.0, average(1.0, 3.0) )
+}
+
+TEST (task1, test2) {
+ASSERT_EQ(1.485, average(6.47, -3.5) )
+}
+
+TEST (task1, test3) {
+ASSERT_EQ(0.0, average(-50.0, 50.0 ))
+}
+
+TEST (task1, test4) {
+ASSERT_EQ(1.414214, calc(2.0) )
+}
+
+TEST (task1, test5) {
+ASSERT_EQ(1.732050, calc(3.0))
+}
