6262980e0cc6608b152019d758345cb8f98576f3
KuznetsovMikhail <KuznetsovMikhail>
2020-09-21 15:22:37 +0300

    Кузнецов Михаил

---
+++ src/task1.h
@@ -0,0 +1,16 @@
+#include <math.h>
+#include <stdio.h>
+#ifndef TASK1_H_INCLUDED
+#define TASK1_H_INCLUDED
+
+double average (double x, double y);
+
+bool good (double guess, double x);
+double improve (double guess, double x);
+
+double iter (double guess, double iter);
+
+double calc (double arg);
+
+
+#endif // TASK1_H_INCLUDED
--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -2,4 +2,20 @@
 
 #include "gtest/gtest.h"
 
+TEST(task1, test1)
+{
+    ASSERT_EQ(2.5,average(2.0, 3.0));
 
+}
+
+TEST(task1, test2)
+{
+     ASSERT_EQ(true,good(1.0, 1.0));
+
+}
+
+TEST(task1, test3)
+{
+     ASSERT_EQ(1.0,improve(1.0, 1.0));
+
+}
