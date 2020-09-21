9ca109f0299d54dd4a0f4415f07f11034f1bb5d6
Schterben <dsbarinov_1@edu.hse.ru>
2020-09-21 16:10:41 +0300

    SpasiboDeduZaGithub

--- src/main1.cpp
+++ src/main1.cpp
@@ -1,12 +1,13 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdlib.h>
 #include "task1.h"
+#include <math.h>
+#include <stdio.h>
 
-	int main()
-	{
-		double result = calc(2.0);
-		printf("Sqrt(%lf)=%lf\n", 2.0, result);
-		return 0;
-	}
+int main()
+{
 
+	double result = calc(2.0);
+
+	printf("Sqrt(%lf) = %lf\n", 2.0, result);
+
+	return 0;
+} 
--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -1,14 +1,7 @@
+#include "gtest/gtest.h"
+
 TEST(task1, test1)
 {
-	ASSERT_NEAR(1.414214, calc(2), 0.1);
-}
+	ASSERT_NEAR(1.414214, calc(2.0), 0.000001);
 
-TEST(task1, test2)
-{
-ASSERT_NEAR(2.0 ,calc(4), 0.1);
-}
-
-TEST(task1, test3)
-{
-ASSERT_NEAR(3.0 ,calc(9), 0.1);
 } 
