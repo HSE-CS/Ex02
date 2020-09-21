1479aabedf5c2e10bec80b301de4169dba858c76
Vladislav-Kanev <71171071+Vladislav-Kanev@users.noreply.github.com>
2020-09-21 15:25:05 +0300

    эмм..

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -2,4 +2,19 @@
 
 #include "gtest/gtest.h"
 
+TEST(task1, test1)
+{
+    ASSERT_EQ(50, calc(2500));
 
+}
+
+TEST(task1, test2)
+{
+    ASSERT_EQ(2, calc(4));
+
+}
+
+TEST(task1, test3)
+{
+    ASSERT_EQ(3, calc(9));
+}
