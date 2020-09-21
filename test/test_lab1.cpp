fc8ebb2fcaccd8c361d3ab29a8cf9ae73f5604b7
Karina Mashanova <karmen.01@inbox.ru>
2020-09-21 16:29:20 +0300

    Second

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -3,8 +3,25 @@
 
 
 
+#include "task1.h"
+#include "gtest/gtest.h"
+
+
 TEST(task1, test1)
 {
-	ASSERT_EQ(2.500000, average(2.0, 3.0));
+	ASSERT_FLOAT_EQ(1.414214, calc(2.0));
+
+}
+
+
+TEST(task1, test2)
+{
+	ASSERT_FLOAT_EQ(3.16227766, calc(3.0));
+
+}
+
+TEST(task1, average1)
+{
+	ASSERT_FLOAT_EQ(2.500000, average(2.0, 3.0));
 
 }
