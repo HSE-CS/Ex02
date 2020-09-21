81ab86eab86412e921d72ea5a172820ba7663706
Мария <71264966+Karcevamd@users.noreply.github.com>
2020-09-21 23:56:00 +0300

    Add files via upload

--- include/task1.h
+++ include/task1.h
@@ -1,11 +1,10 @@
-#ifndef TASK_H
-#define TASK_H
+#define TASK1_H
+#ifdef TASK1_H
 
-double everage(double x, double y);
+double average(double x, double y);
 bool good(double guess, double x);
 double improve(double guess, double x);
 double iter(double guess, double x);
 double calc(double arg);
 
-#endif 
-
+#endif // TASK1_H
