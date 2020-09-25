c93c41630480cf0a7525d9f9db64a4380e10b1ec
Polina <polpan01@mail.ru>
2020-09-26 00:18:03 +0300

    tests

--- src/main1.cpp
+++ src/main1.cpp
@@ -1,12 +1,12 @@
-#include "task1.h"
-#include <math.h>
-#include <stdio.h>
-
 #define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include "task1.h"
 
-int main() 
+
+int main()
 {
-  double result = calc(2.0);
-  printf("Sqrt(%lf)=%lf\n",2.0,result);
-  return 0;
+    double result = calc(2.0);
+    printf("Sqrt(%lf)=%lf\n", 2.0, result);
+    return 0;
 }
