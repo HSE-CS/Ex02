27d2c70b3a0c65ea35f59fda3268e66f47cf9c92
Your NameVadukk <Vadik-Tumakov.com@yandex.ru>
2020-09-27 13:23:21 +0300

    PLEASEEEEE123

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -1,23 +1,15 @@
-#include "../include/task1.h"
-#include "task1.h"
-
-#include "gtest/gtest.h"
-
-TEST(task1, test1)
-{
-	ASSERT_NEAR(1.414214, calc(2.0), 0.00000001);
-	ASSERT_NEAR(1.414214, calc(2), 0.1);
+ASSERT_NEAR(1.414214, calc(2.0), 0.000001);
 
 }
 
 TEST(task1, test2)
 {
-	ASSERT_NEAR(3.0, calc(9), 0.1);
+	ASSERT_NEAR(1.73205, calc(3.0), 0.00001);
 
 }
 
 TEST(task1, test3)
 {
-	ASSERT_NEAR(2.0, calc(4), 0.1);
+	ASSERT_NEAR(2.236068, calc(5.0), 0.000001);
 
 }
