c15b71d5c8902f3744b930ef35ce1b06496de4ee
Your NameVadukk <Vadik-Tumakov.com@yandex.ru>
2020-09-27 13:18:48 +0300

    PLEASEEEEE

--- include/task1.h
+++ include/task1.h
@@ -1,10 +1,17 @@
 #ifndef TASK1_H
 #define TASK1_H
 
+#include <math.h>
+#include <stdio.h>
+
+double average(double x, double y);
+bool good(double guess, double x);
+double improve(double guess, double x);
 double average(double x, double y);
 bool good(double guess, double x);
 double improve(double guess, double x);
 double iter(double guess, double x);
 double calc(double arg);
+
 
 #endif 
--- src/main1.cpp
+++ src/main1.cpp
@@ -1,13 +1,11 @@
-#include "task1.h"
+#include "../include/task1.h"
+
 #include <math.h>
 #include <stdio.h>
 
-int main()
-{
+int main() {
 
 	double result = calc(2.0);
-
 	printf("Sqrt(%lf) = %lf\n", 2.0, result);
-
 	return 0;
 }
