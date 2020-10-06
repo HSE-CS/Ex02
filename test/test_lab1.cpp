594d874c19293150e2d7c3f3ec279e736165e1d5
RBurtsev <r.a.burtsev@mail.ru>
2020-10-06 12:14:19 +0300

    'task2'

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -1,63 +1,54 @@
-#include "task1.h"
+#include "../include/task1.h"
 
 #include "gtest/gtest.h"
 
-TEST(task1, test1)
-{
-    ASSERT_DOUBLE_EQ(22.3607,calc(500));
 
+TEST(calc, test1)
+{
+    ASSERT_FLOAT_EQ(7.0, calc(49.0));
 }
 
-TEST(task1, test2)
+TEST(calc, test2)
 {
-     ASSERT_DOUBLE_EQ(1,calc(1));
-	
+    ASSERT_FLOAT_EQ(1.4142135, calc(2.0));
 }
 
-TEST(task1, test3)
+TEST(calc, test3)
 {
-     ASSERT_DOUBLE_EQ(1.41421,calc(2));
-	
+    ASSERT_FLOAT_EQ(12.0, calc(144.0));
 }
 
-TEST(task1, test4)
+TEST(calc, test4)
 {
-    ASSERT_DOUBLE_EQ(70.7107,calc(5000));
-
+    ASSERT_FLOAT_EQ(2.345208, calc(5.5));
 }
 
-TEST(task1, test5)
+TEST(average, test1)
 {
-    ASSERT_DOUBLE_EQ(31.607,calc(999));
-
+    ASSERT_FLOAT_EQ(4.0, average(1.0, 7.0));
 }
 
-TEST(task1, test6)
+TEST(average, test2)
 {
-    ASSERT_DOUBLE_EQ(300,calc(90000));
-
+    ASSERT_FLOAT_EQ(4.5, average(3.5, 5.5));
 }
 
-TEST(task1, test7)
+TEST(improve, test1)
 {
-    ASSERT_DOUBLE_EQ(12,calc(144));
-
+    ASSERT_DOUBLE_EQ(1.750000, improve(3.0, 4.0));
 }
 
-TEST(task1, test8)
+TEST(improve, test2)
 {
-    ASSERT_DOUBLE_EQ(19.2094,calc(369));
-
+    ASSERT_NEAR(2.320238, improve(4.2, 1.85), 0.00001);
 }
 
-TEST(task1, test9)
+TEST(iter, test1)
 {
-    ASSERT_DOUBLE_EQ(29.7993,calc(888));
-
+    ASSERT_NEAR(4.0, iter(1.0, 16.0), 0.0000000001);
 }
 
-TEST(task1, test10)
+TEST(iter, test2)
 {
-    ASSERT_DOUBLE_EQ(351.363,calc(123456));
-
+    ASSERT_NEAR(2.828427, iter(2.0, 8.0), 0.000001);
 }
