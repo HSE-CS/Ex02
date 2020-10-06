0b1f22f823afbf26da53a3a9edbc5833df816a95
Andrej Kostin <andrej_kostin_2001@mail.ru>
2020-10-06 23:20:01 +0300

    Kostin Andrej 19SE-2 6 test version 2

--- src/main1.cpp
+++ src/main1.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
     cout << "hi,bro" << endl;
-    double a = calc(3.0);
+    double a = improve(2.0, 9.0);
     cout<<"Sqrt("<<3.0<<")"<<"="<<a<<endl;
     return 0;
 }
