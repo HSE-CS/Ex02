076be0698557b0a4fd1bdbcc31b5dad01a7b3791
Julia Rogozyan <juliarogozyan@MacBook-Air-Julia.local>
2020-10-12 15:26:35 +0300

    mmm

--- include/task1.hpp
+++ include/task1.hpp
@@ -5,9 +5,14 @@
 //  Created by Julia Rogozyan on 12.10.2020.
 //  Copyright © 2020 Julia Rogozyan. All rights reserved.
 //
+ 
+#ifndef TASK1_H
+#define TASK1_H
 
 double average(double x, double y);
 bool good(double guess, double x);
 double improve(double guess, double x);
 double iter(double guess, double x);
-double calc(double arg); 
+double calc(double arg);
+
+#endif
