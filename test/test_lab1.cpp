2326a263e9ceafc01b922e39b76bfb7b57aa244d
AlexeusC4ever <62253233+AlexeusC4ever@users.noreply.github.com>
2020-10-12 19:28:07 +0300

    Add files via upload

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -1,5 +1,40 @@
+#pragma once
+#include <iostream>
 #include "task1.h"
+#include <cmath>
+#include <random>
+#include <cstdlib>
+#include <vector>
 
-#include "gtest/gtest.h"
+using namespace std;
+
+//double GenerateNumber() {
+//	double cost;
+//	cost = rand()%1000;
+//	return cost;
+//}
+
+vector<double> GenerateNumber(mt19937& generator, int length) {
+	vector<double> costs(length);
+	for (double& cost : costs) {
+		cost = uniform_real_distribution<>(0, 10000)(generator);
+	}
+	return costs;
+}
+
+double GenerateDelta(mt19937& generator) {
+	double cost = 0;
+	cost = uniform_real_distribution<>(0, 100000)(generator)*0.0000001;
+	return cost;
+}
+
+bool checkresult(double result, double number, double delta) {
+	if (abs(result - sqrt(number)) < delta) {
+		/*double a = abs(result - sqrt(number));
+		cout << a << endl;*/
+		return true;
+	}
+	return false;
+}
 
 
