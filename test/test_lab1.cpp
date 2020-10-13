c6d50395e1881e3209e90782874c0f57e329a950
Alexey Trutnev <66214960+alexgiving@users.noreply.github.com>
2020-10-13 12:53:08 +0300

    Update test_lab1.cpp

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -1,6 +1,5 @@
 
 #include "task1.h"
-
 #include "gtest/gtest.h"
 
 TEST(task1, test1)
@@ -30,5 +29,23 @@ ASSERT_NEAR(25.0 ,average(50,0), 0.1);
 TEST(task1, test5)
 {
 ASSERT_NEAR(2.0 ,average(2,2), 0.1);
+
+}
+
+TEST(task1, test6)
+{
+ASSERT_NEAR(6.0 ,average(5,7), 0.1);
+
+}
+
+TEST(task1, test7)
+{
+ASSERT_NEAR(5.0 ,average(9,1), 0.1);
+
+}
+
+TEST(task1, test8)
+{
+ASSERT_NEAR(3.5 ,average(4,3), 0.1);
 
 }
