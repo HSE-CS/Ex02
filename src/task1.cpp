9e88f73a8cb9d29edc6a3af866253379baf82854
Alexander Grachev <s89519011368@yandex.ru>
2020-10-16 20:19:36 +0300

    changed

--- src/task1.cpp
+++ src/task1.cpp
@@ -1,12 +1,13 @@
 #include <math.h>
+#include <cmath>
 
 double delta = 0.00000001;
 
 double average(double x, double y){
     return (x + y) / 2.0;
 }
-bool good(double guess, double x){
-    return abs(guess * guess - x) <delta;
+bool good(double guess, double x) {
+    return fabs(guess * guess - x) < delta;
 }
 double improve(double guess, double x){
     return average(guess, x / guess);
