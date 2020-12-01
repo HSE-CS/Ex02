858f837c83a8e4173b9a44d3c5474b3f34d7b6d5
Nikita Ziganshin <63304439+Liberator-1775@users.noreply.github.com>
2020-12-01 19:02:54 +0300

    Add files via upload

--- src/task1.cpp
+++ src/task1.cpp
@@ -1,27 +1,28 @@
-//
-// Created by nikita on 15.09.2020.
-//
-
 #include "task1.h"
 
-double average(double x,double y) {
-    return (x + y) / 2.0;
+double delta = 0.00000001;
+
+double average(double x, double y) {
+    return (x+y)/2.0;
 }
 
-bool good(double guess,double x) {
-    return fabs(guess * guess - x) < delta;
+bool good(double guess, double x) {
+    return abs(guess * guess - x) < delta;
 }
 
-double improve(double guess,double x) {
+double improve(double guess, double x) {
     return average(guess, x / guess);
 }
 
 double iter(double guess, double x) {
-    if (good(guess, x))
+    if(good(guess, x)) {
         return guess;
-    else
+    }
+    else {
         return iter(improve(guess, x), x);
+    }
 }
+
 double calc(double arg) {
     return iter(1.0, arg);
 }
