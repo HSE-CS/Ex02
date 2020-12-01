fde5cfbc4a4db58e1518e2a1c9d7f2cb4f61d20e
Nikita Ziganshin <63304439+Liberator-1775@users.noreply.github.com>
2020-12-01 19:02:18 +0300

    Add files via upload

--- include/task1.h
+++ include/task1.h
@@ -1,19 +1,15 @@
-//
-// Created by nikita on 15.09.2020.
-//
-
-#ifndef EX02_TASK1_H
-#define EX02_TASK1_H
+#ifndef TASK1_TASK1_H
+#define TASK1_TASK1_H
 
 #include <cmath>
 #include <iostream>
 
-double delta = 0.00000001;
+using namespace std;
 
-double average (double x,double y);
-bool good(double guess,double x);
-double improve (double guess,double x);
+double average(double x, double y);
+bool good(double guess, double x);
+double improve(double guess, double x);
 double iter(double guess, double x);
 double calc(double arg);
 
-#endif //EX02_TASK1_H
+#endif //TASK1_TASK1_H
