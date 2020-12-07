f986274c2d54c31aca9d718bd97f400466f0b63f
nikit <nikita201721@gmail.com>
2020-12-07 11:25:13 +0300

    Changed tests

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -5,12 +5,12 @@
 TEST(ex02, test1)
 {
     double result = average(1.0, 2.0);
-    EXPECT_EQ(1.5, result);
+    EXPECT_DOUBLE_EQ(1.5, result);
 }
 TEST(ex02, test2)
 {
     double result = average(0.12345678, 8.7654321);
-    EXPECT_EQ(4.44444444, result);
+    EXPECT_DOUBLE_EQ(4.44444444, result);
 }
 TEST(ex02, test3)
 {
@@ -20,32 +20,32 @@ TEST(ex02, test3)
 TEST(ex02, test4)
 {
     double result = improve(114.0, 8.0);
-    EXPECT_EQ(57.0351, result);
+    EXPECT_DOUBLE_EQ(57.0351, result);
 }
 TEST(ex02, test5)
 {
     double result = improve(14.0, 12.0);
-    EXPECT_EQ(7.42857, result);
+    EXPECT_DOUBLE_EQ(7.42857, result);
 }
 TEST(ex02, test6)
 {
     double result = iter(1.0, 4.0);
-    EXPECT_EQ(2, result);
+    EXPECT_DOUBLE_EQ(2, result);
 }
 TEST(ex02, test7)
 {
     double result = calc(9.0);
-    EXPECT_EQ(3, result);
+    EXPECT_DOUBLE_EQ(3, result);
 }
 TEST(ex02, test8)
 {
     double result = calc(2.0);
-    EXPECT_EQ(1.41421, result);
+    EXPECT_DOUBLE_EQ(1.41421, result);
 }
 TEST(ex02, test9)
 {
     double result = average(2.0, 4.0);
-    EXPECT_EQ(3.0, result);
+    EXPECT_DOUBLE_EQ(3.0, result);
 }
 TEST(ex02, test10)
 {
